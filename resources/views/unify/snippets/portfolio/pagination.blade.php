f2489a9cd769ac6315f2c2be60a452ede1cec90c
Ross Edlin <rossedlin@gmail.com>
2017-09-01 17:01:25 +0700

    add - first portfolio item (unfinished)

---
+++ resources/views/unify/snippets/portfolio/pagination.blade.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ross Edlin
+ * Date: 01/09/2017
+ * Time: 16:56
+ */
+?>
+<!-- Pagination -->
+<nav class="text-center" aria-label="Page Navigation">
+    <ul class="list-inline">
+        <li class="list-inline-item float-left hidden-xs-down">
+            <a class="u-pagination-v1__item u-pagination-v1-4 g-brd-gray-light-v3 g-brd-primary--hover g-rounded-50 g-pa-7-16"
+               href="#" aria-label="Previous">
+                <span aria-hidden="true">
+                  <i class="fa fa-angle-left g-mr-5"></i> Previous
+                </span>
+                <span class="sr-only">Previous</span>
+            </a>
+        </li>
+        <li class="list-inline-item">
+            <a class="u-pagination-v1__item u-pagination-v1-4 u-pagination-v1-4--active g-rounded-50 g-pa-7-14"
+               href="#">1</a>
+        </li>
+        <li class="list-inline-item">
+            <a class="u-pagination-v1__item u-pagination-v1-4 g-rounded-50 g-pa-7-14" href="#">2</a>
+        </li>
+        <li class="list-inline-item float-right hidden-xs-down">
+            <a class="u-pagination-v1__item u-pagination-v1-4 g-brd-gray-light-v3 g-brd-primary--hover g-rounded-50 g-pa-7-16"
+               href="#" aria-label="Next">
+                <span aria-hidden="true">
+                  Next <i class="fa fa-angle-right g-ml-5"></i>
+                </span>
+                <span class="sr-only">Next</span>
+            </a>
+        </li>
+    </ul>
+</nav>
+<!-- End Pagination -->
