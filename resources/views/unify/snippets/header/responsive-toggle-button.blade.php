9e8fcb8d282b56a7b3c0a34b7e5b425abeb9d7a1
Ross Edlin <rossedlin@gmail.com>
2017-09-02 19:59:53 +0700

    add - responsive button snippet

---
+++ resources/views/unify/snippets/header/responsive-toggle-button.blade.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ross Edlin
+ * Date: 02/09/2017
+ * Time: 19:44
+ */
+?>
+<!-- Responsive Toggle Button -->
+<button class="navbar-toggler navbar-toggler-right btn g-line-height-1 g-brd-none g-pa-0 g-pos-abs g-right-0"
+        type="button"
+        aria-label="Toggle navigation"
+        aria-expanded="false"
+        aria-controls="navBar"
+        data-toggle="collapse"
+        data-target="#navBar">
+
+    <span class="hamburger hamburger--slider">
+      <span class="hamburger-box">
+        <span class="hamburger-inner"></span>
+      </span>
+    </span>
+
+</button>
+<!-- End Responsive Toggle Button -->
