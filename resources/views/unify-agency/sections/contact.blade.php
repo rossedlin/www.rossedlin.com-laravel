13ee72e11f30bfdfcf867fe069891e1250b381eb
Ross Edlin <rossedlin@gmail.com>
2017-09-04 12:55:04 +0700

    contact "me"

--- resources/views/unify-agency/sections/contact.blade.php
+++ resources/views/unify-agency/sections/contact.blade.php
@@ -10,7 +10,9 @@
     <div class="g-py-30 g-py-80--md">
         <div class="container">
             <div class="text-uppercase u-heading-v2-4--bottom g-brd-primary">
-                <h4 class="g-line-height-1 g-font-weight-600 g-font-size-12 g-color-primary g-mb-20">Contact us</h4>
+                <h4 class="g-line-height-1 g-font-weight-600 g-font-size-12 g-color-primary g-mb-20">
+                    Contact me
+                </h4>
                 <h2 class="u-heading-v2__title g-font-weight-600 g-font-size-30 g-font-size-40--md g-theme-color-gray-dark-v1">
                     Get in touch</h2>
             </div>
