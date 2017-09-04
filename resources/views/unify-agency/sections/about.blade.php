45c2645cd6901c4e9786c5a20957ecdc5eb46513
Ross Edlin <rossedlin@gmail.com>
2017-09-04 16:22:18 +0700

    update about us page massively

--- resources/views/unify-agency/sections/about.blade.php
+++ resources/views/unify-agency/sections/about.blade.php
@@ -2,87 +2,118 @@
 /**
  * Created by PhpStorm.
  * User: Ross Edlin
- * Date: 04/09/2017
- * Time: 11:24
+ * Date: 31/08/2017
+ * Time: 16:51
  */
 ?>
-<!-- Section Content -->
-<section id="about" class="g-pos-rel">
-    <!-- Info Blocks -->
-    <div class="row align-items-stretch no-gutters">
-        <div class="col-md-6">
-            <!-- Article -->
-            <article class="h-100 g-bg-img-hero"
-                     style="background-image: url(/theme/unify-agency/assets/img-temp/900x390/img1.jpg);">
-                <div class="d-flex text-left text-lg-right h-100 g-theme-bg-gray-dark-v2 g-bg-primary-dark-v1-opacity-0_9--hover g-py-30 g-py-90--md g-px-15 g-px-30--md g-pr-170--lg g-transition-0_3 g-transition--ease-in">
-                    <div class="align-self-center">
-                        <h3 class="h5 text-uppercase g-font-weight-700 g-font-secondary g-color-white g-mb-12">We
-                            love our customers</h3>
-                        <p class="g-color-white-opacity-0_7 mb-0">Nulla cursus orci sed ipsum scelerisque volutpat.
-                            Integer quis dapibus leo, maximus ultrices dui. Mauris facilisis, ex sed scelerisque
-                            bibendum, tellus leo pharetra augue, sed iaculis felis neque quis magna.</p>
-                    </div>
-                </div>
-            </article>
-            <!-- End Article -->
-        </div>
+<!-- Figure -->
+<section class="g-pt-100 g-pb-40">
+    <div class="container">
+        <figure class="row">
+            <!-- Figure Image -->
+            <div class="col-md-8 offset-md-2 col-lg-4 offset-lg-0 g-mb-30">
+                <img class="w-100 rounded border" src="/img/about/ross-edlin-headshot-400-450.jpg" alt="Image Description">
+            </div>
+            <!-- End Figure Image -->
 
-        <div class="col-md-6">
-            <!-- Article -->
-            <article class="h-100 g-bg-img-hero"
-                     style="background-image: url(/theme/unify-agency/assets/img-temp/900x390/img2.jpg);">
-                <div class="d-flex h-100 g-theme-bg-gray-dark-v1 g-bg-primary-dark-v1-opacity-0_9--hover g-py-30 g-py-90--md g-px-15 g-px-30--md g-pl-170--lg g-transition-0_3 g-transition--ease-in">
-                    <div class="align-self-center">
-                        <h3 class="h5 text-uppercase g-font-weight-700 g-font-secondary g-color-white g-mb-12">We
-                            are creative</h3>
-                        <p class="g-color-white-opacity-0_7 mb-0">Nunc sed justo semper, gravida orci at, malesuada
-                            ipsum. Morbi at faucibus enim, ac pharetra purus. Phasellus id tellus eu nisi porta
-                            maximus. Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+            <!-- Figure Body -->
+            <div class="col-lg-8">
+                <div class="d-flex justify-content-between g-mb-10">
+                    <div class="g-mb-20">
+                        <h4 class="h5 g-mb-5">Ross Edlin</h4>
+                        <em class="d-block g-font-style-normal g-font-size-default text-uppercase g-color-primary">
+                            Senior Web Developer
+                        </em>
+                    </div>
+
+                    <!-- Figure Social Icons -->
+                    <ul class="list-inline">
+                        <li class="list-inline-item g-mx-2">
+                            <a class="u-icon-v1 u-icon-size--sm u-icon-slide-up--hover g-color-gray-light-v1 g-bg-gray-light-v5 g-color-gray-light-v1--hover rounded-circle"
+                               href="https://www.facebook.com/rossedlin" target="_blank">
+                                <i class="g-font-size-default g-line-height-1 u-icon__elem-regular fa fa-facebook"></i>
+                                <i class="g-font-size-default g-line-height-0_8 u-icon__elem-hover fa fa-facebook"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item g-mx-2">
+                            <a class="u-icon-v1 u-icon-size--sm u-icon-slide-up--hover g-color-gray-light-v1 g-bg-gray-light-v5 g-color-gray-light-v1--hover rounded-circle"
+                               href="https://www.instagram.com/rossedlin/" target="_blank">
+                                <i class="g-font-size-default g-line-height-1 u-icon__elem-regular fa fa-instagram"></i>
+                                <i class="g-font-size-default g-line-height-0_8 u-icon__elem-hover fa fa-instagram"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item g-mx-2">
+                            <a class="u-icon-v1 u-icon-size--sm u-icon-slide-up--hover g-color-gray-light-v1 g-bg-gray-light-v5 g-color-gray-light-v1--hover rounded-circle"
+                               href="https://www.linkedin.com/in/rossedlin/" target="_blank">
+                                <i class="g-font-size-default g-line-height-1 u-icon__elem-regular fa fa-linkedin"></i>
+                                <i class="g-font-size-default g-line-height-0_8 u-icon__elem-hover fa fa-linkedin"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item g-mx-2">
+                            <a class="u-icon-v1 u-icon-size--sm u-icon-slide-up--hover g-color-gray-light-v1 g-bg-gray-light-v5 g-color-gray-light-v1--hover rounded-circle"
+                               href="https://github.com/rossedlin/" target="_blank">
+                                <i class="g-font-size-default g-line-height-1 u-icon__elem-regular fa fa-github"></i>
+                                <i class="g-font-size-default g-line-height-0_8 u-icon__elem-hover fa fa-github"></i>
+                            </a>
+                        </li>
+                    </ul>
+                    <!-- End Figure Social Icons -->
+                </div>
+
+                <!-- Figure Info -->
+                <div class="g-mb-50">
+                    <p>I am an ambitious workaholic, but apart from that, pretty simple person. Whether it's branding,
+                        print, UI + UX I've got you covered. I strive to figure out the right solutions for your look to
+                        stand out amongst the rest.</p>
+                    <p>As creatives, it's important that I strive to do work outside of obligation. This lets us stay ahead
+                        of the curve for our clients and internal projects. At the end of the day, it's important to not let
+                        being busy distract us from having fun. Smiling, laughing, and hanging helps us work together to
+                        achieve this.</p>
+                </div>
+                <!-- End Info -->
+
+                <div class="row">
+                    <div class="col-sm-6">
+                        <!-- Progress Bar v2 -->
+                        <h4 class="h6 g-mb-5">PHP (Laravel, Zend) - 8 Years</h4>
+                        <div class="js-hr-progress-bar progress rounded-0 g-height-4 mb-5">
+                            <div class="js-hr-progress-bar-indicator progress-bar" role="progressbar" style="width: 80%;"
+                                 aria-valuenow="76" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                        <!-- End Progress Bar v2 -->
+
+                        <!-- Progress Bar v2 -->
+                        <h4 class="h6 g-mb-5">Web Design - 8 Years</h4>
+                        <div class="js-hr-progress-bar progress rounded-0 g-height-4 mb-5">
+                            <div class="js-hr-progress-bar-indicator progress-bar" role="progressbar" style="width: 80%;"
+                                 aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                        <!-- End Progress Bar v2 -->
+
+
+                    </div>
+
+                    <div class="col-sm-6">
+                        <!-- Progress Bar v2 -->
+                        <h4 class="h6 g-mb-5">jQuery - 80%</h4>
+                        <div class="js-hr-progress-bar progress rounded-0 g-height-4 mb-5">
+                            <div class="js-hr-progress-bar-indicator progress-bar" role="progressbar" style="width: 80%;"
+                                 aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                        <!-- End Progress Bar v2 -->
+
+                        <!-- Progress Bar v2 -->
+                        <h4 class="h6 g-mb-5">CSS - 4 Years</h4>
+                        <div class="js-hr-progress-bar progress rounded-0 g-height-4 mb-5">
+                            <div class="js-hr-progress-bar-indicator progress-bar" role="progressbar" style="width: 40%;"
+                                 aria-valuenow="76" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                        <!-- End Progress Bar v2 -->
                     </div>
                 </div>
-            </article>
-            <!-- End Article -->
-        </div>
+            </div>
+            <!-- End Figure Body -->
+        </figure>
     </div>
-
-    <div class="row align-items-stretch no-gutters">
-        <div class="col-md-6">
-            <!-- Article -->
-            <article class="h-100 g-bg-img-hero"
-                     style="background-image: url(/theme/unify-agency/assets/img-temp/900x390/img3.jpg);">
-                <div class="d-flex text-left text-lg-right h-100 g-theme-bg-gray-dark-v1 g-bg-primary-dark-v1-opacity-0_9--hover g-py-30 g-py-90--md g-px-15 g-px-30--md g-pr-170--lg g-transition-0_3 g-transition--ease-in">
-                    <div class="align-self-center">
-                        <h3 class="h5 text-uppercase g-font-weight-700 g-font-secondary g-color-white g-mb-12">We
-                            are pragmatic</h3>
-                        <p class="g-color-white-opacity-0_7 mb-0">Aliquam dignissim, leo sit amet pulvinar dapibus,
-                            neque sapien bibendum massa, a efficitur massa mauris a augue. Suspendisse faucibus
-                            facilisis porttitor. Suspendisse tristique arcu vel</p>
-                    </div>
-                </div>
-            </article>
-            <!-- End Article -->
-        </div>
-
-        <div class="col-md-6">
-            <!-- Article -->
-            <article class="h-100 g-bg-img-hero"
-                     style="background-image: url(/theme/unify-agency/assets/img-temp/900x390/img4.jpg);">
-                <div class="d-flex h-100 g-theme-bg-gray-dark-v2 g-bg-primary-dark-v1-opacity-0_9--hover g-py-30 g-py-90--md g-px-15 g-px-30--md g-pl-170--lg g-transition-0_3 g-transition--ease-in">
-                    <div class="align-self-center">
-                        <h3 class="h5 text-uppercase g-font-weight-700 g-font-secondary g-color-white g-mb-12">We
-                            are professional</h3>
-                        <p class="g-color-white-opacity-0_7 mb-0">Mauris aliquet, magna nec gravida interdum, magna
-                            nibh fringilla nulla, eget egestas sapien orci eget tellus. Pellentesque vulputate
-                            posuere libero a varius. Duis feugiat nisl id arcu posuere dapibus eu sed augue.</p>
-                    </div>
-                </div>
-            </article>
-            <!-- End Article -->
-        </div>
-    </div>
-
-    <img class="hidden-sm-down g-width-100--md g-width-200--lg g-height-100--md g-height-200--lg g-absolute-centered"
-         src="/theme/unify-agency/assets/img-temp/230x230/img1.jpg" alt="Image description">
-    <!-- End Info Blocks -->
 </section>
-<!-- End Section Content -->
+<!-- End Figure -->
