c1524da91c4a6779d4e34e0c488511dee00dba46
Ross Edlin <rossedlin@gmail.com>
2017-09-04 11:32:56 +0700

    add - extract footer

---
+++ resources/views/unify-agency/sections/testimonials.blade.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ross Edlin
+ * Date: 04/09/2017
+ * Time: 11:30
+ */
+?>
+<!-- Section Content -->
+<section id="testimonials" class="g-theme-bg-gray-light-v4">
+    <div class="container text-center g-max-width-770 g-py-30 g-py-70--md">
+        <div class="u-heading-v8-2 g-mb-40">
+            <h2 class="text-uppercase u-heading-v8__title g-font-weight-700 g-font-secondary g-font-size-26 g-color-gray-dark-v2 g-mb-25">
+                Want to join our team?</h2>
+            <p class="mb-0">Sed eget aliquet nisl. Proin laoreet accumsan nisl non vestibulum. Donec molestie, lorem
+                nec sollicitudin elementum, mi justo posuere lectus, vitae ullamcorper orci mi vel massa.
+                Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        </div>
+
+        <a class="btn u-btn-primary btn-md text-uppercase g-font-weight-700 g-font-size-12 rounded-0 g-px-25 g-py-10 mb-0"
+           href="#">Send your resume</a>
+    </div>
+
+    <div class="container-fluid px-0">
+        <div class="row no-gutters">
+            <!-- Testimonials Block -->
+            <section class="col-lg-6 g-theme-bg-gray-dark-v1 g-py-30 g-py-70--md g-px-30">
+                <div class="js-carousel"
+                     data-infinite="true"
+                     data-arrows-classes="hidden-xs-down u-arrow-v1 g-pos-abs g-right-0 g-top-0 g-width-50 g-height-50 g-rounded-50x g-font-size-18 g-color-white g-theme-bg-gray-dark-v2 g-bg-primary-dark-v1--hover g-mt-15 g-transition-0_2 g-transition--ease-in"
+                     data-arrow-left-classes="fa fa-angle-left g-mr-60"
+                     data-arrow-right-classes="fa fa-angle-right">
+                    <div class="js-slide g-color-white">
+                        <!-- Testimonial Block -->
+                        <div class="media g-mb-25">
+                            <div class="media-left g-pr-20">
+                                <img class="media-object rounded-circle"
+                                     src="/theme/unify-agency/assets/img-temp/80x80/img1.jpg"
+                                     alt="Image Description">
+                            </div>
+
+                            <div class="media-body align-self-center text-uppercase">
+                                <h4 class="g-font-weight-700 g-font-size-default g-color-white g-mb-3">Melissa
+                                    Jordany</h4>
+                                <em class="g-font-weight-700 g-font-style-normal g-font-size-10 g-theme-color-gray-light-v3 g-mb-0">Gray
+                                    Consultion Corp.</em>
+                            </div>
+                        </div>
+
+                        <blockquote class="g-theme-color-gray-light-v3 mb-0">Ut augue diam, lacinia fringilla erat
+                            eu, vehicula commodo quam. Aliquam eget accumsan ligula. Maecenas sit amet consectetur
+                            lectus. Suspendisse commodo et magna non pulvinar. Quisque et ultricies sem, et
+                            vulputate dui. Morbi aliquam leo id ipsum tempus mollis.
+                        </blockquote>
+                        <!-- End Testimonial Block -->
+                    </div>
+
+                    <div class="js-slide g-color-white">
+                        <!-- Testimonial Block -->
+                        <div class="media g-mb-25">
+                            <div class="media-left g-pr-20">
+                                <img class="media-object rounded-circle"
+                                     src="/theme/unify-agency/assets/img-temp/80x80/img2.jpg"
+                                     alt="Image Description">
+                            </div>
+
+                            <div class="media-body align-self-center text-uppercase">
+                                <h4 class="g-font-weight-700 g-font-size-default g-color-white g-mb-3">Joe
+                                    Armstrong</h4>
+                                <em class="g-font-weight-700 g-font-style-normal g-font-size-10 g-theme-color-gray-light-v3 g-mb-0">Blue
+                                    Yellow Sky Corp.</em>
+                            </div>
+                        </div>
+
+                        <blockquote class="g-theme-color-gray-light-v3 mb-0">Ut augue diam, lacinia fringilla erat
+                            eu, vehicula commodo quam. Aliquam eget accumsan ligula. Maecenas sit amet consectetur
+                            lectus. Suspendisse commodo et magna non pulvinar. Quisque et ultricies sem, et
+                            vulputate dui. Morbi aliquam leo id ipsum tempus mollis.
+                        </blockquote>
+                        <!-- End Testimonial Block -->
+                    </div>
+                </div>
+            </section>
+            <!-- End Testimonials Block -->
+
+            <section class="col-lg-6 d-flex g-theme-bg-gray-dark-v2 g-py-30 g-py-70--md g-px-30">
+                <div id="carouselCus1" class="js-carousel w-100 align-self-center"
+                     data-autoplay="true"
+                     data-slides-show="4">
+                    <div class="js-slide">
+                        <img class="img-fluid mx-auto" src="/theme/unify-agency/assets/img-temp/200x150/img1.png"
+                             alt="Image description">
+                    </div>
+
+                    <div class="js-slide">
+                        <img class="img-fluid mx-auto" src="/theme/unify-agency/assets/img-temp/200x150/img2.png"
+                             alt="Image description">
+                    </div>
+
+                    <div class="js-slide">
+                        <img class="img-fluid mx-auto" src="/theme/unify-agency/assets/img-temp/200x150/img3.png"
+                             alt="Image description">
+                    </div>
+
+                    <div class="js-slide">
+                        <img class="img-fluid mx-auto" src="/theme/unify-agency/assets/img-temp/200x150/img4.png"
+                             alt="Image description">
+                    </div>
+
+                    <div class="js-slide">
+                        <img class="img-fluid mx-auto" src="/theme/unify-agency/assets/img-temp/200x150/img5.png"
+                             alt="Image description">
+                    </div>
+
+                    <div class="js-slide">
+                        <img class="img-fluid mx-auto" src="/theme/unify-agency/assets/img-temp/200x150/img6.png"
+                             alt="Image description">
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+</section>
+<!-- End Section Content -->
