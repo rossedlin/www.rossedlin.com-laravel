b740a2e8b918b3dc87b01eb3227cb57df1f03538
Ross Edlin <rossedlin@gmail.com>
2017-09-04 11:25:30 +0700

    add - extract about

---
+++ resources/views/unify-agency/sections/about.blade.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ross Edlin
+ * Date: 04/09/2017
+ * Time: 11:24
+ */
+?>
+<!-- Section Content -->
+<section id="about" class="g-pos-rel">
+    <!-- Info Blocks -->
+    <div class="row align-items-stretch no-gutters">
+        <div class="col-md-6">
+            <!-- Article -->
+            <article class="h-100 g-bg-img-hero"
+                     style="background-image: url(/theme/unify-agency/assets/img-temp/900x390/img1.jpg);">
+                <div class="d-flex text-left text-lg-right h-100 g-theme-bg-gray-dark-v2 g-bg-primary-dark-v1-opacity-0_9--hover g-py-30 g-py-90--md g-px-15 g-px-30--md g-pr-170--lg g-transition-0_3 g-transition--ease-in">
+                    <div class="align-self-center">
+                        <h3 class="h5 text-uppercase g-font-weight-700 g-font-secondary g-color-white g-mb-12">We
+                            love our customers</h3>
+                        <p class="g-color-white-opacity-0_7 mb-0">Nulla cursus orci sed ipsum scelerisque volutpat.
+                            Integer quis dapibus leo, maximus ultrices dui. Mauris facilisis, ex sed scelerisque
+                            bibendum, tellus leo pharetra augue, sed iaculis felis neque quis magna.</p>
+                    </div>
+                </div>
+            </article>
+            <!-- End Article -->
+        </div>
+
+        <div class="col-md-6">
+            <!-- Article -->
+            <article class="h-100 g-bg-img-hero"
+                     style="background-image: url(/theme/unify-agency/assets/img-temp/900x390/img2.jpg);">
+                <div class="d-flex h-100 g-theme-bg-gray-dark-v1 g-bg-primary-dark-v1-opacity-0_9--hover g-py-30 g-py-90--md g-px-15 g-px-30--md g-pl-170--lg g-transition-0_3 g-transition--ease-in">
+                    <div class="align-self-center">
+                        <h3 class="h5 text-uppercase g-font-weight-700 g-font-secondary g-color-white g-mb-12">We
+                            are creative</h3>
+                        <p class="g-color-white-opacity-0_7 mb-0">Nunc sed justo semper, gravida orci at, malesuada
+                            ipsum. Morbi at faucibus enim, ac pharetra purus. Phasellus id tellus eu nisi porta
+                            maximus. Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                    </div>
+                </div>
+            </article>
+            <!-- End Article -->
+        </div>
+    </div>
+
+    <div class="row align-items-stretch no-gutters">
+        <div class="col-md-6">
+            <!-- Article -->
+            <article class="h-100 g-bg-img-hero"
+                     style="background-image: url(/theme/unify-agency/assets/img-temp/900x390/img3.jpg);">
+                <div class="d-flex text-left text-lg-right h-100 g-theme-bg-gray-dark-v1 g-bg-primary-dark-v1-opacity-0_9--hover g-py-30 g-py-90--md g-px-15 g-px-30--md g-pr-170--lg g-transition-0_3 g-transition--ease-in">
+                    <div class="align-self-center">
+                        <h3 class="h5 text-uppercase g-font-weight-700 g-font-secondary g-color-white g-mb-12">We
+                            are pragmatic</h3>
+                        <p class="g-color-white-opacity-0_7 mb-0">Aliquam dignissim, leo sit amet pulvinar dapibus,
+                            neque sapien bibendum massa, a efficitur massa mauris a augue. Suspendisse faucibus
+                            facilisis porttitor. Suspendisse tristique arcu vel</p>
+                    </div>
+                </div>
+            </article>
+            <!-- End Article -->
+        </div>
+
+        <div class="col-md-6">
+            <!-- Article -->
+            <article class="h-100 g-bg-img-hero"
+                     style="background-image: url(/theme/unify-agency/assets/img-temp/900x390/img4.jpg);">
+                <div class="d-flex h-100 g-theme-bg-gray-dark-v2 g-bg-primary-dark-v1-opacity-0_9--hover g-py-30 g-py-90--md g-px-15 g-px-30--md g-pl-170--lg g-transition-0_3 g-transition--ease-in">
+                    <div class="align-self-center">
+                        <h3 class="h5 text-uppercase g-font-weight-700 g-font-secondary g-color-white g-mb-12">We
+                            are professional</h3>
+                        <p class="g-color-white-opacity-0_7 mb-0">Mauris aliquet, magna nec gravida interdum, magna
+                            nibh fringilla nulla, eget egestas sapien orci eget tellus. Pellentesque vulputate
+                            posuere libero a varius. Duis feugiat nisl id arcu posuere dapibus eu sed augue.</p>
+                    </div>
+                </div>
+            </article>
+            <!-- End Article -->
+        </div>
+    </div>
+
+    <img class="hidden-sm-down g-width-100--md g-width-200--lg g-height-100--md g-height-200--lg g-absolute-centered"
+         src="/theme/unify-agency/assets/img-temp/230x230/img1.jpg" alt="Image description">
+    <!-- End Info Blocks -->
+</section>
+<!-- End Section Content -->
