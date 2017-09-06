a9d56a2c83bebaa708e092b252af798f9b7d2227
Ross Edlin <rossedlin@gmail.com>
2017-09-06 12:22:00 +0700

    extract header form new-age

---
+++ resources/views/theme/new-age/header/default.blade.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ross Edlin
+ * Date: 06/09/2017
+ * Time: 12:20
+ */
+?>
+<header class="masthead" style="padding: 100px 0 40px 0;">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-lg-7">
+                <div class="header-content mx-auto text-center">
+                    <h1 class="mb-5">
+                        I'm <span>Ross Edlin</span>, a web developer, check out my
+                        <a href="http://www.cuttingweb.co.uk/portfolio" target="_blank">portfolio here</a>
+                        or have a browse of my business website.
+                    </h1>
+                    <a href="http://www.cuttingweb.co.uk/" target="_blank" class="btn btn-outline btn-xl">See My
+                        Business Website</a>
+                </div>
+            </div>
+            <div class="col-xs-12 col-lg-5 text-center">
+                <div class="container">
+                    <img id="img-ross-edlin" class="img-responsive img-thumbnail img-circle"
+                         src="img/header/ross-edlin-casual-500x500.jpg">
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
--- resources/views/theme/new-age/index.blade.php
+++ resources/views/theme/new-age/index.blade.php
@@ -76,29 +76,7 @@
     </div>
 </nav>
 
-<header class="masthead" style="padding: 100px 0 40px 0;">
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-lg-7">
-                <div class="header-content mx-auto text-center">
-                    <h1 class="mb-5">
-                        I'm <span>Ross Edlin</span>, a web developer, check out my
-                        <a href="http://www.cuttingweb.co.uk/portfolio" target="_blank">portfolio here</a>
-                        or have a browse of my business website.
-                    </h1>
-                    <a href="http://www.cuttingweb.co.uk/" target="_blank" class="btn btn-outline btn-xl">See My
-                        Business Website</a>
-                </div>
-            </div>
-            <div class="col-xs-12 col-lg-5 text-center">
-                <div class="container">
-                    <img id="img-ross-edlin" class="img-responsive img-thumbnail img-circle"
-                         src="img/header/ross-edlin-casual-500x500.jpg">
-                </div>
-            </div>
-        </div>
-    </div>
-</header>
+@include('theme.new-age.header.default')
 
 <?php /*
 <section class="download bg-primary text-center" id="download">
