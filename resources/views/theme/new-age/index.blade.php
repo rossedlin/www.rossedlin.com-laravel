fa66c9d6dcba0dacda38e5d609c085e55c6c166f
Ross Edlin <rossedlin@gmail.com>
2017-09-06 12:16:02 +0700

    cut from rossedlin.com-landing-page

--- resources/views/theme/new-age/index.blade.php
+++ resources/views/theme/new-age/index.blade.php
@@ -2,8 +2,8 @@
 /**
  * Created by PhpStorm.
  * User: Ross Edlin
- * Date: 28/08/2017
- * Time: 22:27
+ * Date: 18/08/2017
+ * Time: 16:20
  */
 ?>
         <!DOCTYPE html>
@@ -14,25 +14,36 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="author" content="Ross Edlin">
 
-    <title>New Age - Start Bootstrap Theme</title>
+    <title>Ross Edlin</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="theme/new-age/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom fonts for this template -->
-    <link rel="stylesheet" href="theme/new-age/vendor/font-awesome/css/font-awesome.min.css">
-    <link rel="stylesheet" href="theme/new-age/vendor/simple-line-icons/css/simple-line-icons.css">
+    <link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="vendor/simple-line-icons/css/simple-line-icons.css">
     <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
 
-    <!-- Plugin CSS -->
-    <link rel="stylesheet" href="theme/new-age/device-mockups/device-mockups.min.css">
-
     <!-- Custom styles for this template -->
-    <link href="theme/new-age/css/new-age.min.css" rel="stylesheet">
+    <link href="css/new-age.css?v=<?= Utils::getFileModifyTime(__DIR__ . '/css/new-age.css') ?>" rel="stylesheet">
+
+    <style>
+        .img-circle
+        {
+            border-radius: 50%;
+        }
+
+        #img-ross-edlin
+        {
+            -webkit-box-shadow: 0px 2px 5px 0px rgba(0, 0, 0, 0.4);
+            -moz-box-shadow:    0px 2px 5px 0px rgba(0, 0, 0, 0.4);
+            box-shadow:         0px 2px 5px 0px rgba(0, 0, 0, 0.4);
+        }
+    </style>
 
 </head>
 
@@ -40,168 +51,168 @@
 
 <!-- Navigation -->
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-    <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
-                data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
-                aria-label="Toggle navigation">
-            Menu
-            <i class="fa fa-bars"></i>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="#download">Download</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="#features">Features</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
-                </li>
-            </ul>
-        </div>
+    <a class="navbar-brand" href="#page-top">Ross Edlin</a>
+    <button class="navbar-toggler navbar-toggler-right"
+            type="button" data-toggle="collapse"
+            data-target="#navbarResponsive"
+            aria-controls="navbarResponsive"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+        Menu
+        <i class="fa fa-bars"></i>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="http://www.cuttingweb.co.uk/" target="_blank">Business Website</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="http://www.cuttingweb.co.uk/cv" target="_blank">C.V.</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="http://www.cuttingweb.co.uk/contact" target="_blank">Contact</a>
+            </li>
+        </ul>
     </div>
 </nav>
 
-<header class="masthead">
-    <div class="container h-100">
-        <div class="row h-100">
-            <div class="col-lg-7 my-auto">
-                <div class="header-content mx-auto">
-                    <h1 class="mb-5">New Age is an app landing page that will help you beautifully showcase your new
-                        mobile app, or anything else!</h1>
-                    <a href="#download" class="btn btn-outline btn-xl js-scroll-trigger">Start Now for Free!</a>
+<header class="masthead" style="padding: 100px 0 40px 0;">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-lg-7">
+                <div class="header-content mx-auto text-center">
+                    <h1 class="mb-5">
+                        I'm <span>Ross Edlin</span>, a web developer, check out my
+                        <a href="http://www.cuttingweb.co.uk/portfolio" target="_blank">portfolio here</a>
+                        or have a browse of my business website.
+                    </h1>
+                    <a href="http://www.cuttingweb.co.uk/" target="_blank" class="btn btn-outline btn-xl">See My
+                        Business Website</a>
                 </div>
             </div>
-            <div class="col-lg-5 my-auto">
-                <div class="device-container">
-                    <div class="device-mockup iphone6_plus portrait white">
-                        <div class="device">
-                            <div class="screen">
-                                <!-- Demo image for screen mockup, you can put an image here, some HTML, an animation, video, or anything else! -->
-                                <img src="theme/new-age/img/demo-screen-1.jpg" class="img-fluid" alt="">
-                            </div>
-                            <div class="button">
-                                <!-- You can hook the "home button" to some JavaScript events or just remove it -->
-                            </div>
-                        </div>
-                    </div>
+            <div class="col-xs-12 col-lg-5 text-center">
+                <div class="container">
+                    <img id="img-ross-edlin" class="img-responsive img-thumbnail img-circle"
+                         src="img/ross-edlin-casual-500.jpg">
                 </div>
             </div>
         </div>
     </div>
 </header>
 
+<?php /*
 <section class="download bg-primary text-center" id="download">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 mx-auto">
-                <h2 class="section-heading">Discover what all the buzz is about!</h2>
-                <p>Our app is available on any mobile device! Download now to get started!</p>
-                <div class="badges">
-                    <a class="badge-link" href="#"><img src="theme/new-age/img/google-play-badge.svg" alt=""></a>
-                    <a class="badge-link" href="#"><img src="theme/new-age/img/app-store-badge.svg" alt=""></a>
-                </div>
-            </div>
-        </div>
-    </div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8 mx-auto">
+				<h2 class="section-heading">Discover what all the buzz is about!</h2>
+				<p>Our app is available on any mobile device! Download now to get started!</p>
+				<div class="badges">
+					<a class="badge-link" href="#"><img src="img/google-play-badge.svg" alt=""></a>
+					<a class="badge-link" href="#"><img src="img/app-store-badge.svg" alt=""></a>
+				</div>
+			</div>
+		</div>
+	</div>
 </section>
+*/ ?>
 
+<?php /*
 <section class="features" id="features">
-    <div class="container">
-        <div class="section-heading text-center">
-            <h2>Unlimited Features, Unlimited Fun</h2>
-            <p class="text-muted">Check out what you can do with this app theme!</p>
-            <hr>
-        </div>
-        <div class="row">
-            <div class="col-lg-4 my-auto">
-                <div class="device-container">
-                    <div class="device-mockup iphone6_plus portrait white">
-                        <div class="device">
-                            <div class="screen">
-                                <!-- Demo image for screen mockup, you can put an image here, some HTML, an animation, video, or anything else! -->
-                                <img src="theme/new-age/img/demo-screen-1.jpg" class="img-fluid" alt="">
-                            </div>
-                            <div class="button">
-                                <!-- You can hook the "home button" to some JavaScript events or just remove it -->
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-8 my-auto">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <div class="feature-item">
-                                <i class="icon-screen-smartphone text-primary"></i>
-                                <h3>Device Mockups</h3>
-                                <p class="text-muted">Ready to use HTML/CSS device mockups, no Photoshop required!</p>
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="feature-item">
-                                <i class="icon-camera text-primary"></i>
-                                <h3>Flexible Use</h3>
-                                <p class="text-muted">Put an image, video, animation, or anything else in the
-                                    screen!</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <div class="feature-item">
-                                <i class="icon-present text-primary"></i>
-                                <h3>Free to Use</h3>
-                                <p class="text-muted">As always, this theme is free to download and use for any
-                                    purpose!</p>
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="feature-item">
-                                <i class="icon-lock-open text-primary"></i>
-                                <h3>Open Source</h3>
-                                <p class="text-muted">Since this theme is MIT licensed, you can use it commercially!</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+	<div class="container">
+		<div class="section-heading text-center">
+			<h2>Unlimited Features, Unlimited Fun</h2>
+			<p class="text-muted">Check out what you can do with this app theme!</p>
+			<hr>
+		</div>
+		<div class="row">
+			<div class="col-lg-4 my-auto">
+				<div class="device-container">
+					<div class="device-mockup iphone6_plus portrait white">
+						<div class="device">
+							<div class="screen">
+								<!-- Demo image for screen mockup, you can put an image here, some HTML, an animation, video, or anything else! -->
+								<img src="img/demo-screen-1.jpg" class="img-fluid" alt="">
+							</div>
+							<div class="button">
+								<!-- You can hook the "home button" to some JavaScript events or just remove it -->
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-8 my-auto">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-lg-6">
+							<div class="feature-item">
+								<i class="icon-screen-smartphone text-primary"></i>
+								<h3>Device Mockups</h3>
+								<p class="text-muted">Ready to use HTML/CSS device mockups, no Photoshop required!</p>
+							</div>
+						</div>
+						<div class="col-lg-6">
+							<div class="feature-item">
+								<i class="icon-camera text-primary"></i>
+								<h3>Flexible Use</h3>
+								<p class="text-muted">Put an image, video, animation, or anything else in the
+									screen!</p>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-lg-6">
+							<div class="feature-item">
+								<i class="icon-present text-primary"></i>
+								<h3>Free to Use</h3>
+								<p class="text-muted">As always, this theme is free to download and use for any
+									purpose!</p>
+							</div>
+						</div>
+						<div class="col-lg-6">
+							<div class="feature-item">
+								<i class="icon-lock-open text-primary"></i>
+								<h3>Open Source</h3>
+								<p class="text-muted">Since this theme is MIT licensed, you can use it commercially!</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </section>
+*/ ?>
 
+<?php /*
 <section class="cta">
-    <div class="cta-content">
-        <div class="container">
-            <h2>Stop waiting.<br>Start building.</h2>
-            <a href="#contact" class="btn btn-outline btn-xl js-scroll-trigger">Let's Get Started!</a>
-        </div>
-    </div>
-    <div class="overlay"></div>
+	<div class="cta-content">
+		<div class="container">
+			<h2>Stop waiting.<br>Start building.</h2>
+			<a href="#contact" class="btn btn-outline btn-xl">Let's Get Started!</a>
+		</div>
+	</div>
+	<div class="overlay"></div>
 </section>
+*/ ?>
 
 <section class="contact bg-primary" id="contact">
     <div class="container">
-        <h2>We
-            <i class="fa fa-heart"></i>
-            new friends!</h2>
+        <h2>Connect with me</h2>
         <ul class="list-inline list-social">
-            <li class="list-inline-item social-twitter">
-                <a href="#">
-                    <i class="fa fa-twitter"></i>
-                </a>
-            </li>
             <li class="list-inline-item social-facebook">
-                <a href="#">
+                <a href="https://www.facebook.com/rossedlin" target="_blank">
                     <i class="fa fa-facebook"></i>
                 </a>
             </li>
-            <li class="list-inline-item social-google-plus">
-                <a href="#">
-                    <i class="fa fa-google-plus"></i>
+            <li class="list-inline-item social-linkedin">
+                <a href="https://www.linkedin.com/in/rossedlin/" target="_blank">
+                    <i class="fa fa-linkedin"></i>
+                </a>
+            </li>
+            <li class="list-inline-item social-github">
+                <a href="https://github.com/rossedlin/" target="_blank">
+                    <i class="fa fa-github"></i>
                 </a>
             </li>
         </ul>
@@ -210,31 +221,21 @@
 
 <footer>
     <div class="container">
-        <p>&copy; 2017 Start Bootstrap. All Rights Reserved.</p>
-        <ul class="list-inline">
-            <li class="list-inline-item">
-                <a href="#">Privacy</a>
-            </li>
-            <li class="list-inline-item">
-                <a href="#">Terms</a>
-            </li>
-            <li class="list-inline-item">
-                <a href="#">FAQ</a>
-            </li>
-        </ul>
+        <p>&copy; <?= date('Y') ?> <a href="http://www.cuttingweb.co.uk/" target="_blank"
+                                      style="color: rgba(255, 255, 255, 0.5);">Cutting Web.</a> All Rights Reserved.</p>
     </div>
 </footer>
 
 <!-- Bootstrap core JavaScript -->
-<script src="theme/new-age/vendor/jquery/jquery.min.js"></script>
-<script src="theme/new-age/vendor/popper/popper.min.js"></script>
-<script src="theme/new-age/vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/popper/popper.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
 
 <!-- Plugin JavaScript -->
-<script src="theme/new-age/vendor/jquery-easing/jquery.easing.min.js"></script>
+<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
 
 <!-- Custom scripts for this template -->
-<script src="theme/new-age/js/new-age.min.js"></script>
+<script src="js/new-age.min.js"></script>
 
 </body>
 
