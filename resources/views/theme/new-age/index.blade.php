80d0512427ae6ea4e006ef3c2969837073065e23
Ross Edlin <rossedlin@gmail.com>
2017-09-06 14:36:43 +0700

    extract new-age theme contact page

--- resources/views/theme/new-age/index.blade.php
+++ resources/views/theme/new-age/index.blade.php
@@ -151,28 +151,7 @@
 </section>
 */ ?>
 
-<section class="contact bg-primary" id="contact">
-    <div class="container">
-        <h2>Connect with me</h2>
-        <ul class="list-inline list-social">
-            <li class="list-inline-item social-facebook">
-                <a href="https://www.facebook.com/rossedlin" target="_blank">
-                    <i class="fa fa-facebook"></i>
-                </a>
-            </li>
-            <li class="list-inline-item social-linkedin">
-                <a href="https://www.linkedin.com/in/rossedlin/" target="_blank">
-                    <i class="fa fa-linkedin"></i>
-                </a>
-            </li>
-            <li class="list-inline-item social-github">
-                <a href="https://github.com/rossedlin/" target="_blank">
-                    <i class="fa fa-github"></i>
-                </a>
-            </li>
-        </ul>
-    </div>
-</section>
+@include('theme.new-age.sections.contact')
 
 <footer>
     <div class="container">
---
+++ resources/views/theme/new-age/sections/contact.blade.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ross Edlin
+ * Date: 06/09/2017
+ * Time: 14:35
+ */
+?>
+<section class="contact bg-primary" id="contact">
+    <div class="container">
+        <h2>Connect with me</h2>
+        <ul class="list-inline list-social">
+            <li class="list-inline-item social-facebook">
+                <a href="https://www.facebook.com/rossedlin" target="_blank">
+                    <i class="fa fa-facebook"></i>
+                </a>
+            </li>
+            <li class="list-inline-item social-linkedin">
+                <a href="https://www.linkedin.com/in/rossedlin/" target="_blank">
+                    <i class="fa fa-linkedin"></i>
+                </a>
+            </li>
+            <li class="list-inline-item social-github">
+                <a href="https://github.com/rossedlin/" target="_blank">
+                    <i class="fa fa-github"></i>
+                </a>
+            </li>
+        </ul>
+    </div>
+</section>
