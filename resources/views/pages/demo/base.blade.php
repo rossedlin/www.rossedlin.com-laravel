998aba7fa44d4f50b9de29b45c1fae978f909d72
Ross Edlin <rossedlin@gmail.com>
2017-09-06 16:34:13 +0700

    add - base blade

---
+++ resources/views/pages/demo/base.blade.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ross Edlin
+ * Date: 06/09/2017
+ * Time: 16:26
+ */
+        ?>
+
+@extends('theme.unify-agency.layouts.default')
+
+@section('content')
+    <!-- Base Section -->
+    <section class="container">
+        <div class="row">
+            <h1>Hey, I'm a title</h1>
+        </div>
+
+        <div class="row">
+            <h1>Hey, I'm a title</h1>
+        </div>
+
+        <div class="row">
+            <p>And some content here</p>
+        </div>
+    </section>
+    <!-- End Base Section -->
+@stop
