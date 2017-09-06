2109c85bba65e6d8edec50c641784899ba23d241
Ross Edlin <rossedlin@gmail.com>
2017-09-06 15:08:25 +0700

    add - unify footer

---
+++ resources/views/theme/unify-agency/footer/unify.blade.php
@@ -0,0 +1,156 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ross Edlin
+ * Date: 31/08/2017
+ * Time: 15:16
+ */
+?>
+<!-- Footer -->
+<footer class="footer g-py-60">
+    <div class="container">
+        <div class="row">
+            <!-- Footer Content -->
+            <div class="col-lg-3 col-md-6 g-mb-40 g-mb-0--lg">
+                <div class="u-heading-v2-3--bottom g-brd-white-opacity-0_8 g-mb-20">
+                    <h2 class="u-heading-v2__title h6 text-uppercase mb-0">About Us</h2>
+                </div>
+
+                <p>About Unify dolor sit amet, consectetur adipiscing elit. Maecenas eget nisl id libero tincidunt
+                    sodales.</p>
+            </div>
+            <!-- End Footer Content -->
+
+            <!-- Footer Content -->
+            <div class="col-lg-3 col-md-6 g-mb-40 g-mb-0--lg">
+                <div class="u-heading-v2-3--bottom g-brd-white-opacity-0_8 g-mb-20">
+                    <h2 class="u-heading-v2__title h6 text-uppercase mb-0">Latest Posts</h2>
+                </div>
+
+                <article>
+                    <h3 class="h6 g-mb-2">
+                        <a class="g-color-white--hover" href="#">Incredible template</a>
+                    </h3>
+                    <div class="small g-color-white-opacity-0_6">May 8, 2017</div>
+                </article>
+
+                <hr class="g-brd-white-opacity-0_1 g-my-10">
+
+                <article>
+                    <h3 class="h6 g-mb-2">
+                        <a class="g-color-white--hover" href="#">New features</a>
+                    </h3>
+                    <div class="small g-color-white-opacity-0_6">June 23, 2017</div>
+                </article>
+
+                <hr class="g-brd-white-opacity-0_1 g-my-10">
+
+                <article>
+                    <h3 class="h6 g-mb-2">
+                        <a class="g-color-white--hover" href="#">New terms and
+                            conditions</a>
+                    </h3>
+                    <div class="small g-color-white-opacity-0_6">September 15, 2017</div>
+                </article>
+            </div>
+            <!-- End Footer Content -->
+
+            <!-- Footer Content -->
+            <div class="col-lg-3 col-md-6 g-mb-40 g-mb-0--lg">
+                <div class="u-heading-v2-3--bottom g-brd-white-opacity-0_8 g-mb-20">
+                    <h2 class="u-heading-v2__title h6 text-uppercase mb-0">Useful Links</h2>
+                </div>
+
+                <nav class="text-uppercase1">
+                    <ul class="list-unstyled g-mt-minus-10 mb-0">
+                        <li class="g-pos-rel g-brd-bottom g-brd-white-opacity-0_1 g-py-10">
+                            <h4 class="h6 g-pr-20 mb-0">
+                                <a class="g-color-white--hover" href="/about">About Us</a>
+                                <i class="fa fa-angle-right g-absolute-centered--y g-right-0"></i>
+                            </h4>
+                        </li>
+                        <li class="g-pos-rel g-brd-bottom g-brd-white-opacity-0_1 g-py-10">
+                            <h4 class="h6 g-pr-20 mb-0">
+                                <a class="g-color-white--hover"
+                                   href="/portfolio">Portfolio</a>
+                                <i class="fa fa-angle-right g-absolute-centered--y g-right-0"></i>
+                            </h4>
+                        </li>
+                        <li class="g-pos-rel g-brd-bottom g-brd-white-opacity-0_1 g-py-10">
+                            <h4 class="h6 g-pr-20 mb-0">
+                                <a class="g-color-white--hover" href="/services">Our
+                                    Services</a>
+                                <i class="fa fa-angle-right g-absolute-centered--y g-right-0"></i>
+                            </h4>
+                        </li>
+                        <li class="g-pos-rel g-py-10">
+                            <h4 class="h6 g-pr-20 mb-0">
+                                <a class="g-color-white--hover" href="/contact">Contact Us</a>
+                                <i class="fa fa-angle-right g-absolute-centered--y g-right-0"></i>
+                            </h4>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <!-- End Footer Content -->
+
+            <!-- Footer Content -->
+            <div class="col-lg-3 col-md-6">
+                <div class="u-heading-v2-3--bottom g-brd-white-opacity-0_8 g-mb-20">
+                    <h2 class="u-heading-v2__title h6 text-uppercase mb-0">Our Contacts</h2>
+                </div>
+
+                <address class="g-bg-no-repeat g-font-size-12 mb-0"
+                         style="background-image: url(/theme/unify/assets/img/maps/map2.png);">
+
+                <?php /*
+					<!-- Location -->
+					<div class="d-flex g-mb-20">
+						<div class="g-mr-10">
+                    <span class="u-icon-v3 u-icon-size--xs g-bg-white-opacity-0_1 g-color-white-opacity-0_6">
+                      <i class="fa fa-map-marker"></i>
+                    </span>
+						</div>
+						<p class="mb-0">795 Folsom Ave, Suite 600, <br> San Francisco, CA 94107 795</p>
+					</div>
+					<!-- End Location -->
+                    */ ?>
+
+                <?php /*
+					<!-- Phone -->
+					<div class="d-flex g-mb-20">
+						<div class="g-mr-10">
+                    <span class="u-icon-v3 u-icon-size--xs g-bg-white-opacity-0_1 g-color-white-opacity-0_6">
+                      <i class="fa fa-phone"></i>
+                    </span>
+						</div>
+						<p class="mb-0" style="margin-top: 8px;">
+							<a class="g-color-white--hover" href="tel:+442032907017">(+44) 020 3290 7017</a>
+						</p>
+					</div>
+					<!-- End Phone -->
+ 					*/ ?>
+
+                <!-- Email and Website -->
+                    <div class="d-flex g-mb-20">
+                        <div class="g-mr-10">
+                    <span class="u-icon-v3 u-icon-size--xs g-bg-white-opacity-0_1 g-color-white-opacity-0_6">
+                      <i class="fa fa-globe"></i>
+                    </span>
+                        </div>
+                        <p class="mb-0">
+                            <a class="g-color-white--hover"
+                               href="mailto:contact@cuttingweb.co.uk">contact@cuttingweb.co.uk</a>
+                            <br>
+                            <a class="g-color-white--hover"
+                               href="http://www.cuttingweb.co.uk">www.cuttingweb.co.uk</a>
+                        </p>
+                    </div>
+                    <!-- End Email and Website -->
+                </address>
+            </div>
+            <!-- End Footer Content -->
+        </div>
+    </div>
+</footer>
+<!-- End Footer -->
