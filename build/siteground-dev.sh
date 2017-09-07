accb19ab0e2f51dbae5ace72a0367335e84c3063
Ross Edlin <rossedlin@gmail.com>
2017-09-07 15:35:58 +0700

    rename to .com

--- build/siteground-dev.sh
+++ build/siteground-dev.sh
@@ -1,29 +1,29 @@
-cd ~/rossedlin.co.uk/
-echo "Install -> laravel-dev.rossedlin.co.uk"
+cd ~/rossedlin.com/
+echo "Install -> laravel-dev.rossedlin.com"
 
 #Cleanup
 rm -R -f old_laravel-dev
-rm -R -f www.rossedlin.co.uk-laravel
+rm -R -f www.rossedlin.com-laravel
 
 #Git
 git clone -b dev https://bitbucket.org/cuttingweb/www.rossedlin.com-laravel
 
-if [ -d ~/rossedlin.co.uk/www.rossedlin.co.uk-laravel ]; then
+if [ -d ~/rossedlin.com/www.rossedlin.com-laravel ]; then
 
     #Composer
-    cd ~/rossedlin.co.uk/www.rossedlin.co.uk-laravel
+    cd ~/rossedlin.com/www.rossedlin.com-laravel
     composer install
 
     #Environment File
-    cd ~/rossedlin.co.uk/
-    cp ~/rossedlin.co.uk/laravel-dev/.env ~/rossedlin.co.uk/www.rossedlin.co.uk-laravel/.env
+    cd ~/rossedlin.com/
+    cp ~/rossedlin.com/laravel-dev/.env ~/rossedlin.com/www.rossedlin.com-laravel/.env
 
     #Folders
-    cd ~/rossedlin.co.uk/
+    cd ~/rossedlin.com/
     mv laravel-dev old_laravel-dev
-    mv www.rossedlin.co.uk-laravel laravel-dev
+    mv www.rossedlin.com-laravel laravel-dev
 
     #Permissions
-    chmod 755 -R ~/rossedlin.co.uk/laravel-dev
+    chmod 755 -R ~/rossedlin.com/laravel-dev
 
 fi
