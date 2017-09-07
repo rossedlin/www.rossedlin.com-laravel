43ccc3b68f314cd33ffd155d5cfc416fc55a097b
Ross Edlin <rossedlin@gmail.com>
2017-09-07 13:43:50 +0700

    add - head

---
+++ resources/views/theme/unify-consulting/head/default.blade.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ross Edlin
+ * Date: 07/09/2017
+ * Time: 12:51
+ */
+?>
+<!-- Title -->
+<title>Ross Edlin</title>
+
+<!-- Required Meta Tags Always Come First -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+
+<!-- Favicon -->
+<link rel="shortcut icon" href="/favicon.ico">
+
+<!-- Google Fonts -->
+<link rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700%7CPT+Sans%7CMerriweather">
+
+<!-- CSS Global Compulsory -->
+<link rel="stylesheet" href="/theme/unify/assets/vendor/bootstrap/bootstrap.min.css">
+<link rel="stylesheet" href="/theme/unify/assets/vendor/icon-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" href="/theme/unify/assets/vendor/icon-line-pro/style.css">
+
+<!-- CSS Implementing Plugins -->
+<link rel="stylesheet" href="/theme/unify/assets/vendor/icon-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" href="/theme/unify/assets/vendor/icon-hs/style.css">
+<link rel="stylesheet" href="/theme/unify/assets/vendor/hamburgers/hamburgers.min.css">
+<link rel="stylesheet" href="/theme/unify/assets/vendor/dzsparallaxer/dzsparallaxer.css">
+<link rel="stylesheet" href="/theme/unify/assets/vendor/dzsparallaxer/dzsscroller/scroller.css">
+<link rel="stylesheet" href="/theme/unify/assets/vendor/dzsparallaxer/advancedscroller/plugin.css">
+<link rel="stylesheet" href="/theme/unify/assets/vendor/slick-carousel/slick/slick.css">
+<link rel="stylesheet" href="/theme/unify/assets/vendor/cubeportfolio-full/cubeportfolio/css/cubeportfolio.min.css">
+<link rel="stylesheet" href="/theme/unify/assets/vendor/fancybox/jquery.fancybox.css">
+
+<!-- CSS Template -->
+<link rel="stylesheet" href="/theme/unify-consulting/css/styles.op-consulting.css">
+
+<!-- Revolution Slider -->
+<link rel="stylesheet"
+      href="/theme/unify/assets/vendor/revolution-slider/revolution/fonts/pe-icon-7-stroke/css/pe-icon-7-stroke.css">
+<link rel="stylesheet" href="/theme/unify/assets/vendor/revolution-slider/revolution/css/settings.css">
+<link rel="stylesheet" href="/theme/unify/assets/vendor/revolution-slider/revolution/css/layers.css">
+<link rel="stylesheet" href="/theme/unify/assets/vendor/revolution-slider/revolution/css/navigation.css">
+<link rel="stylesheet"
+      href="/theme/unify/assets/vendor/revolution-slider/revolution-addons/typewriter/css/typewriter.css">
+
+<!-- CSS Customization -->
+<link rel="stylesheet" href="/theme/unify/assets/css/custom.css">
