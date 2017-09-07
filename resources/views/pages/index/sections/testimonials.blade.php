e6fdd59d30c3b7db0caa1d87f7132c1dfa49c410
Ross Edlin <rossedlin@gmail.com>
2017-09-07 13:51:23 +0700

    add - testimonials

---
+++ resources/views/pages/index/sections/testimonials.blade.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ross Edlin
+ * Date: 07/09/2017
+ * Time: 13:50
+ */
+?>
+<!-- Testimonials -->
+<div class="container g-pt-70 g-pb-100">
+    <div id="carouselCus2"
+         class="js-carousel u-carousel-indicators-v32 g-pos-stc g-width-200 text-center mx-auto g-mb-20"
+         data-infinite="true"
+         data-center-mode="true"
+         data-slides-show="3"
+         data-is-thumbs="true"
+         data-nav-for="#carouselCus1">
+        <div class="js-slide d-flex text-center g-opacity-1 g-cursor-pointer g-px-13 g-py-50">
+            <img class="u-shadow-v22 u-carousel-indicators-v32-img g-width-40 g-height-40 g-brd-around g-brd-2 g-brd-transparent rounded-circle"
+                 src="/theme/unify-consulting/img-temp/100x100/img3.jpg" alt="Image Description">
+        </div>
+
+        <div class="js-slide d-flex text-center g-opacity-1 g-cursor-pointer g-px-13 g-py-50">
+            <img class="u-shadow-v22 u-carousel-indicators-v32-img g-width-40 g-height-40 g-brd-around g-brd-2 g-brd-transparent rounded-circle"
+                 src="/theme/unify-consulting/img-temp/100x100/img2.jpg" alt="Image Description">
+        </div>
+
+        <div class="js-slide d-flex text-center g-opacity-1 g-cursor-pointer g-px-13 g-py-50">
+            <img class="u-shadow-v22 u-carousel-indicators-v32-img g-width-40 g-height-40 g-brd-around g-brd-2 g-brd-transparent rounded-circle"
+                 src="/theme/unify-consulting/img-temp/100x100/img1.jpg" alt="Image Description">
+        </div>
+
+        <div class="js-slide d-flex text-center g-opacity-1 g-cursor-pointer g-px-13 g-py-50">
+            <img class="u-shadow-v22 u-carousel-indicators-v32-img g-width-40 g-height-40 g-brd-around g-brd-2 g-brd-transparent rounded-circle"
+                 src="/theme/unify-consulting/img-temp/100x100/img4.jpg" alt="Image Description">
+        </div>
+    </div>
+
+    <div id="carouselCus1" class="js-carousel g-px-50 g-px-200--lg"
+         data-infinite="true"
+         data-arrows-classes="u-arrow-v1 g-absolute-centered--y g-width-45 g-height-45 g-font-size-30 g-color-main-light-v1 g-color-primary--hover"
+         data-arrow-left-classes="fa fa-angle-left g-left-0 g-left-40--lg g-mt-minus-50"
+         data-arrow-right-classes="fa fa-angle-right g-right-0 g-right-40--lg g-mt-minus-50"
+         data-nav-for="#carouselCus2">
+
+        <div class="js-slide text-center">
+            <blockquote class="g-color-text g-font-size-default g-font-size-18--md g-mb-60">Your customer support is
+                the best I have experienced with any theme I have purchased. You have a theme that far exceeds all
+                others. Thanks for providing such a fantastic theme, all your efforts are greatly appreciated on our
+                end.
+            </blockquote>
+            <h4 class="g-font-weight-700 g-font-size-15 g-mb-1">Dick Pottorf</h4>
+            <span class="d-block g-color-text g-font-size-13">Reason: Unify Support</span>
+        </div>
+
+        <div class="js-slide text-center">
+            <blockquote class="g-color-text g-font-size-default g-font-size-18--md g-mb-60">Hi there purchased a
+                couple of days ago and the site looks great, big thanks to the htmlstream guys, they gave me some
+                great help with some fiddly setup issues.
+            </blockquote>
+            <h4 class="g-font-weight-700 g-font-size-15 g-mb-1">Bastien Rojanawisut</h4>
+            <span class="d-block g-color-text g-font-size-13">Reason: Template Quality</span>
+        </div>
+
+        <div class="js-slide text-center">
+            <blockquote class="g-color-text g-font-size-default g-font-size-18--md g-mb-60">Dear Htmlstream team, I
+                just bought the Unify template some weeks ago. The template is really nice and offers quite a large
+                set of options.
+            </blockquote>
+            <h4 class="g-font-weight-700 g-font-size-15 g-mb-1">Alex Pottorf</h4>
+            <span class="d-block g-color-text g-font-size-13">Reason: Template Quality</span>
+        </div>
+
+        <div class="js-slide text-center">
+            <blockquote class="g-color-text g-font-size-default g-font-size-18--md g-mb-60">New Unify template looks
+                great!. Love the multiple layout examples for Shortcodes and the new Show code Copy code snippet
+                feature is brilliant.
+            </blockquote>
+            <h4 class="g-font-weight-700 g-font-size-15 g-mb-1">Mark Mcmanus</h4>
+            <span class="d-block g-color-text g-font-size-13">Reason: Code Quality</span>
+        </div>
+    </div>
+    <!-- End Carousel -->
+</div>
+<!-- End Testimonials -->
