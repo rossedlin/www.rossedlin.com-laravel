6d1a00edd4457d54472b5ebca9491c1dbf073bc6
Ross Edlin <rossedlin@gmail.com>
2017-09-07 13:52:20 +0700

    add - contact

---
+++ resources/views/pages/index/sections/contact.blade.php
@@ -0,0 +1,154 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ross Edlin
+ * Date: 07/09/2017
+ * Time: 13:51
+ */
+?>
+<!-- Contact -->
+<footer id="contact-section" class="g-pos-rel">
+    <!-- Content -->
+    <div class="g-bg-size-cover g-bg-img-hero g-bg-cover g-bg-black-opacity-0_7--after g-pt-120 g-pb-70"
+         style="background-image: url(/theme/unify-consulting/img-temp/1920x1080/img4.jpg);">
+        <div class="container g-z-index-1">
+            <div class="row align-items-center">
+                <div class="col-md-4 g-mb-50">
+                    <h3 class="h4 g-color-white mb-4">Contact Info</h3>
+
+                    <!-- Icon Block -->
+                    <div class="media align-items-center mb-4">
+                        <div class="d-flex">
+                    <span class="u-icon-v1 u-icon-size--sm g-color-white mr-2">
+                      <i class="icon-hotel-restaurant-235 u-line-icon-pro"></i>
+                    </span>
+                        </div>
+                        <div class="media-body">
+                            <p class="g-color-white-opacity-0_6 mb-0">5B Streat, City 50987 New Town US</p>
+                        </div>
+                    </div>
+                    <!-- End Icon Block -->
+
+                    <!-- Icon Block -->
+                    <div class="media align-items-center mb-4">
+                        <div class="d-flex">
+                    <span class="u-icon-v1 u-icon-size--sm g-color-white mr-2">
+                      <i class="icon-communication-033 u-line-icon-pro"></i>
+                    </span>
+                        </div>
+                        <div class="media-body">
+                            <p class="g-color-white-opacity-0_6 mb-0">+32 (0) 333 444 555</p>
+                        </div>
+                    </div>
+                    <!-- End Icon Block -->
+
+                    <!-- Icon Block -->
+                    <div class="media align-items-center g-mb-60">
+                        <div class="d-flex">
+                    <span class="u-icon-v1 u-icon-size--sm g-color-white mr-2">
+                      <i class="icon-communication-062 u-line-icon-pro"></i>
+                    </span>
+                        </div>
+                        <div class="media-body">
+                            <p class="g-color-white-opacity-0_6 mb-0">htmlstream@support.com</p>
+                        </div>
+                    </div>
+                    <!-- End Icon Block -->
+
+                    <!-- Social Icons -->
+                    <h3 class="h4 g-color-white">Social Networks</h3>
+
+                    <ul class="list-inline mb-0">
+                        <li class="list-inline-item g-mx-2">
+                            <a class="u-icon-v1 u-icon-size--sm g-color-white g-bg-white-opacity-0_1 g-bg-primary--hover g-font-size-13 rounded-circle"
+                               href="https://www.facebook.com/htmlstream">
+                                <i class="fa fa-facebook"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item g-mx-2">
+                            <a class="u-icon-v1 u-icon-size--sm g-color-white g-bg-white-opacity-0_1 g-bg-primary--hover g-font-size-13 rounded-circle"
+                               href="https://twitter.com/htmlstream">
+                                <i class="fa fa-twitter"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item g-mx-2">
+                            <a class="u-icon-v1 u-icon-size--sm g-color-white g-bg-white-opacity-0_1 g-bg-primary--hover g-font-size-13 rounded-circle"
+                               href="https://github.com/htmlstream">
+                                <i class="fa fa-github"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item g-mx-2">
+                            <a class="u-icon-v1 u-icon-size--sm g-color-white g-bg-white-opacity-0_1 g-bg-primary--hover g-font-size-13 rounded-circle"
+                               href="https://dribbble.com/htmlstream">
+                                <i class="fa fa-dribbble"></i>
+                            </a>
+                        </li>
+                    </ul>
+                    <!-- End Social Icons -->
+                </div>
+
+                <div class="col-md-8 g-mb-50">
+                    <div class="g-brd-around g-brd-white-opacity-0_6 g-px-45 g-py-60">
+                        <div class="row">
+                            <div class="col-md-4 g-mb-30">
+                                <h2 class="h1 g-color-white">Contact Us</h2>
+                            </div>
+
+                            <div class="col-md-8 g-mb-30">
+                                <!-- Contact Form -->
+                                <form>
+                                    <div class="mb-3">
+                                        <input class="form-control g-brd-none g-brd-bottom g-brd-white g-brd-primary--focus g-color-white g-bg-transparent g-placeholder-gray-light-v5 rounded-0 g-py-13 g-px-0 mb-2"
+                                               type="text" placeholder="Name">
+                                    </div>
+
+                                    <div class="mb-3">
+                                        <input class="form-control g-brd-none g-brd-bottom g-brd-white g-brd-primary--focus g-color-white g-bg-transparent g-placeholder-gray-light-v5 rounded-0 g-py-13 g-px-0 mb-2"
+                                               type="email" placeholder="Email">
+                                    </div>
+
+                                    <div class="mb-4">
+                                            <textarea
+                                                    class="form-control g-brd-none g-brd-bottom g-brd-white g-brd-primary--focus g-color-white g-bg-transparent g-placeholder-gray-light-v5 g-resize-none rounded-0 g-py-13 g-px-0 mb-5"
+                                                    rows="5" placeholder="Message"></textarea>
+                                    </div>
+
+                                    <button class="btn u-btn-primary g-bg-secondary g-color-primary g-color-white--hover g-bg-primary--hover g-font-weight-600 g-font-size-12 g-rounded-30 g-py-15 g-px-35"
+                                            type="submit" role="button">Send Message
+                                    </button>
+                                </form>
+                                <!-- End Contact Form -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Content -->
+
+    <!-- Go To Top -->
+    <a class="js-go-to text-center g-color-main g-color-primary--hover g-left-50x g-ml-minus-100" href="#"
+       data-type="absolute"
+       data-position='{
+             "bottom": 65
+           }'
+       data-offset-top="400"
+       data-compensation="#js-header"
+       data-show-effect="fadeInUp">
+        <svg version="1.1" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg"
+             xmlns:xlink="http://www.w3.org/1999/xlink" width="208px" height="50px">
+            <path fill-rule="evenodd" clip-rule="evenodd" fill="#fff"
+                  d="M111.042,0h-0.085C81.962,0.042,50.96,42.999,6,42.999c-6,0-6,0-6,0v1h214v-1v-0.015C169.917,42.349,139.492,0.042,111.042,0z"/>
+        </svg>
+        <i class="g-font-size-20 g-pos-abs g-bottom-20 g-left-50x g-ml-2 fa fa-angle-double-up"></i>
+    </a>
+    <!-- End Go To Top -->
+
+    <!-- Copyright -->
+    <div class="container text-center g-py-30">
+        <p class="g-font-size-13 mb-0">&#169; 2017 Htmlstream. All Rights Reserved.</p>
+    </div>
+    <!-- End Copyright -->
+</footer>
+<!-- End Contact -->
