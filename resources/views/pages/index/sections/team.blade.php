370d8ab9bf87a0675aad69926187708b27fb5398
Ross Edlin <rossedlin@gmail.com>
2017-09-07 13:45:34 +0700

    add - team

---
+++ resources/views/pages/index/sections/team.blade.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ross Edlin
+ * Date: 07/09/2017
+ * Time: 13:45
+ */
+?>
+<!-- Team -->
+<div class="container g-py-100">
+    <!-- Heading -->
+    <div class="g-max-width-550 text-center mx-auto g-mb-100">
+        <h2 class="text-uppercase g-color-main-light-v1 g-font-weight-600 g-font-size-13 g-letter-spacing-2 mb-4">
+            What are you waiting for?</h2>
+        <h2 class="h3">Join thousands of Unifiers making a difference every day using Unify.</h2>
+    </div>
+    <!-- End Heading -->
+
+    <div class="row g-mx-minus-25 g-mb-40">
+        <div class="col-6 col-lg-3 g-px-25 g-mb-50">
+            <!-- Team -->
+            <div class="text-center">
+                <img class="u-shadow-v29 g-width-110 g-height-110 rounded-circle mb-5"
+                     src="/theme/unify-consulting/img-temp/200x200/img1.jpg" alt="Image Description">
+                <h4 class="h5 g-font-weight-600 mb-1">Alex Taylor</h4>
+                <span class="d-block g-color-primary">Founder</span>
+                <hr class="g-brd-gray-light-v4 g-my-15">
+                <a class="u-link-v5 g-color-text g-color-main--hover g-font-size-13" href="#">alex@gmail.com</a>
+            </div>
+            <!-- End Team -->
+        </div>
+
+        <div class="col-6 col-lg-3 g-px-25 g-mb-50">
+            <!-- Team -->
+            <div class="text-center">
+                <img class="u-shadow-v29 g-width-110 g-height-110 rounded-circle mb-5"
+                     src="/theme/unify-consulting/img-temp/200x200/img3.jpg" alt="Image Description">
+                <h4 class="h5 g-font-weight-600 mb-1">Kate Metu</h4>
+                <span class="d-block g-color-primary">Manager</span>
+                <hr class="g-brd-gray-light-v4 g-my-15">
+                <a class="u-link-v5 g-color-text g-color-main--hover g-font-size-13" href="#">kate@gmail.com</a>
+            </div>
+            <!-- End Team -->
+        </div>
+
+        <div class="col-6 col-lg-3 g-px-25 g-mb-50">
+            <!-- Team -->
+            <div class="text-center">
+                <img class="u-shadow-v29 g-width-110 g-height-110 rounded-circle mb-5"
+                     src="/theme/unify-consulting/img-temp/200x200/img4.jpg" alt="Image Description">
+                <h4 class="h5 g-font-weight-600 mb-1">Daniel Wearne</h4>
+                <span class="d-block g-color-primary">Developer</span>
+                <hr class="g-brd-gray-light-v4 g-my-15">
+                <a class="u-link-v5 g-color-text g-color-main--hover g-font-size-13" href="#">daniel@gmail.com</a>
+            </div>
+            <!-- End Team -->
+        </div>
+
+        <div class="col-6 col-lg-3 g-px-25 g-mb-50">
+            <!-- Team -->
+            <div class="text-center">
+                <img class="u-shadow-v29 g-width-110 g-height-110 rounded-circle mb-5"
+                     src="/theme/unify-consulting/img-temp/200x200/img2.jpg" alt="Image Description">
+                <h4 class="h5 g-font-weight-600 mb-1">Tina Krueger</h4>
+                <span class="d-block g-color-primary">Designer</span>
+                <hr class="g-brd-gray-light-v4 g-my-15">
+                <a class="u-link-v5 g-color-text g-color-main--hover g-font-size-13" href="#">tina@gmail.com</a>
+            </div>
+            <!-- End Team -->
+        </div>
+    </div>
+
+    <div class="text-center">
+        <a class="btn u-btn-primary g-bg-secondary g-color-primary g-color-white--hover g-bg-primary--hover g-font-weight-600 g-font-size-12 g-rounded-30 g-py-15 g-px-35"
+           href="#">Join Unify</a>
+    </div>
+</div>
+<!-- End Team -->
