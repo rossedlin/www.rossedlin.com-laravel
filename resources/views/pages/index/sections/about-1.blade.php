29a3f39023c089244fcab6d75526e34b9bd91b3e
Ross Edlin <rossedlin@gmail.com>
2017-09-07 14:03:52 +0700

    add - about-1

---
+++ resources/views/pages/index/sections/about-1.blade.php
@@ -0,0 +1,172 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ross Edlin
+ * Date: 07/09/2017
+ * Time: 13:11
+ */
+?>
+<!-- About -->
+<div id="about-section">
+    <div class="container g-pt-100">
+        <!-- Heading -->
+        <div class="g-max-width-550 text-center mx-auto g-mb-100">
+            <h1 class="text-uppercase g-color-main-light-v1 g-font-weight-600 g-font-size-13 g-letter-spacing-2 mb-4">
+                About Us</h1>
+            <p class="g-font-size-16">Unify creative technology company providing key digital services. Focused on
+                helping our clients to build a successful business on web and mobile.</p>
+        </div>
+        <!-- End Heading -->
+
+        <!-- Icon Blocks -->
+        <ul class="row list-inline u-info-v9-1 g-pb-70 mb-0">
+            <li class="col-md-4 list-inline-item g-mx-0 g-mb-30">
+                <!-- Icon Blocks -->
+                <div class="u-block-hover text-center g-px-40">
+                    <div class="g-mb-5">
+                  <span class="u-icon-v1 g-width-85 g-height-85 g-color-main g-font-size-50 rounded-circle">
+                    <i class="icon-education-024 u-line-icon-pro"></i>
+                  </span>
+                    </div>
+                    <div class="g-mb-25">
+                  <span class="u-icon-v2 u-shadow-v22 g-width-25 g-height-25 g-color-primary g-bg-white g-font-size-9 rounded-circle">
+                    <i class="fa fa-check"></i>
+                  </span>
+                    </div>
+                    <h3 class="g-color-primary g-font-weight-600 g-font-size-17 text-uppercase mb-3">
+                        <span class="g-color-main g-font-weight-700">01.</span>
+                        Consult
+                    </h3>
+                    <p>We aim high at being focused on building relationships with our clients and community.</p>
+                </div>
+                <!-- End Icon Blocks -->
+            </li>
+
+            <li class="col-md-4 list-inline-item g-mx-0 g-mb-30">
+                <!-- Icon Blocks -->
+                <div class="u-block-hover text-center g-px-40">
+                    <div class="g-mb-5">
+                  <span class="u-icon-v1 g-width-85 g-height-85 g-color-main g-font-size-50 rounded-circle">
+                    <i class="icon-education-073 u-line-icon-pro"></i>
+                  </span>
+                    </div>
+                    <div class="g-mb-25">
+                  <span class="u-icon-v2 u-shadow-v22 g-width-25 g-height-25 g-color-white g-bg-primary g-font-size-9 rounded-circle">
+                    <i class="fa fa-check"></i>
+                  </span>
+                    </div>
+                    <h3 class="g-color-primary g-font-weight-600 g-font-size-17 text-uppercase mb-3">
+                        <span class="g-color-main g-font-weight-700">02.</span>
+                        Create
+                    </h3>
+                    <p>We strive to embrace and drive change in our industry which allows us to keep our clients
+                        relevant.</p>
+                </div>
+                <!-- End Icon Blocks -->
+            </li>
+
+            <li class="col-md-4 list-inline-item g-mx-0 g-mb-30">
+                <!-- Icon Blocks -->
+                <div class="u-block-hover text-center g-px-40">
+                    <div class="g-mb-5">
+                  <span class="u-icon-v1 g-width-85 g-height-85 g-color-main g-font-size-50 rounded-circle">
+                    <i class="icon-communication-180 u-line-icon-pro"></i>
+                  </span>
+                    </div>
+                    <div class="g-mb-25">
+                  <span class="u-icon-v2 u-shadow-v22 g-width-25 g-height-25 g-color-primary g-bg-white g-font-size-9 rounded-circle">
+                    <i class="fa fa-check"></i>
+                  </span>
+                    </div>
+                    <h3 class="g-color-primary g-font-weight-600 g-font-size-17 text-uppercase mb-3">
+                        <span class="g-color-main g-font-weight-700">03.</span>
+                        Release
+                    </h3>
+                    <p>At the end of the day, it's important to not let being busy distract us from having fun.</p>
+                </div>
+                <!-- End Icon Blocks -->
+            </li>
+        </ul>
+        <!-- End Icon Blocks -->
+
+        <!-- Mockup Image -->
+        <img class="img-fluid w-100" src="/theme/unify-consulting/img-temp/1445x750/img1.png"
+             alt="Image Description">
+        <!-- End Mockup Image -->
+
+        <div class="g-pt-100 g-pb-70">
+            <!-- Heading -->
+            <div class="g-max-width-550 text-center mx-auto g-mb-100">
+                <div class="d-inline-block g-width-60 g-height-2 g-bg-secondary-light-v1 mb-3"></div>
+                <h2 class="h3">Our end to end suite includes Customer Support, Responsiveness, 1610+ Ui Elements and
+                    more.</h2>
+            </div>
+            <!-- End Heading -->
+
+            <!-- Vision & Mission -->
+            <div class="g-pos-rel">
+                <div class="row justify-content-between">
+                    <div class="col-lg-5 g-mb-30">
+                        <div class="g-mt-20 mb-5">
+                            <h3 class="h6 text-uppercase g-color-main-light-v1 g-font-weight-600 g-letter-spacing-2 mb-3">
+                                Vision</h3>
+                            <h2 class="mb-4">Our Vision and Mission</h2>
+                            <p class="g-line-height-2">We aim high at being focused on building relationships with
+                                our clients and community. Working together on the daily requires each individual to
+                                let the greater good of the team's work surface above their own ego.</p>
+                        </div>
+
+                        <!-- Icon Block -->
+                        <div class="media mb-4">
+                            <div class="d-flex mr-4">
+                      <span class="u-icon-v3 g-width-50 g-height-50 g-color-bluegray g-bg-bluegray-opacity-0_1 g-font-size-16 rounded-circle">
+                        <i class="icon-communication-114 u-line-icon-pro"></i>
+                      </span>
+                            </div>
+                            <div class="media-body">
+                                <span class="g-font-weight-700">31,500+</span>
+                                <p class="g-text">Happy clients all over the world</p>
+                            </div>
+                        </div>
+                        <!-- End Icon Block -->
+
+                        <!-- Icon Block -->
+                        <div class="media mb-4">
+                            <div class="d-flex mr-4">
+                      <span class="u-icon-v3 g-width-50 g-height-50 g-color-primary g-bg-primary-opacity-0_1 g-font-size-16 rounded-circle">
+                        <i class="icon-finance-091 u-line-icon-pro"></i>
+                      </span>
+                            </div>
+                            <div class="media-body">
+                                <span class="g-font-weight-700">No. 1</span>
+                                <p class="g-text">WrapBootstrap theme of all time</p>
+                            </div>
+                        </div>
+                        <!-- End Icon Block -->
+
+                        <!-- Icon Block -->
+                        <div class="media mb-4">
+                            <div class="d-flex mr-4">
+                      <span class="u-icon-v3 g-width-50 g-height-50 g-color-red g-bg-red-opacity-0_1 g-font-size-16 rounded-circle">
+                        <i class="icon-communication-116 u-line-icon-pro"></i>
+                      </span>
+                            </div>
+                            <div class="media-body">
+                                <span class="g-font-weight-700">1610+</span>
+                                <p class="g-text">UI Elements &amp; Features</p>
+                            </div>
+                        </div>
+                        <!-- End Icon Block -->
+                    </div>
+                </div>
+
+                <div class="col-lg-6 g-pos-abs--lg g-top-0--lg g-right-0--lg g-mb-30">
+                    <img class="w-100 g-width-auto--lg" src="/theme/unify-consulting/img-temp/900x535/img1.png"
+                         alt="Image Description">
+                </div>
+            </div>
+            <!-- End Vision & Mission -->
+        </div>
+    </div>
+</div>
+<!-- End About -->
