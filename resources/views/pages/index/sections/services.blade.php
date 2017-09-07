a2dfcbe218396a768790087d07ef63fbb49c7a2f
Ross Edlin <rossedlin@gmail.com>
2017-09-07 13:46:31 +0700

    add - services

---
+++ resources/views/pages/index/sections/services.blade.php
@@ -0,0 +1,184 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ross Edlin
+ * Date: 07/09/2017
+ * Time: 13:46
+ */
+?>
+<!-- Services -->
+<div id="services-section" class="g-bg-secondary g-py-100">
+    <div class="container">
+        <!-- Heading -->
+        <div class="g-max-width-550 text-center mx-auto g-mb-70">
+            <h2 class="text-uppercase g-color-main-light-v1 g-font-weight-600 g-font-size-13 g-letter-spacing-2 mb-4">
+                Are we a perfect fit?</h2>
+            <h2 class="h3 mb-5">Are Unify's re-usability features<br>perfect for You?</h2>
+        </div>
+        <!-- End Heading -->
+
+        <div class="row justify-content-center g-mb-20">
+            <div class="col-md-6 col-lg-4 g-mb-50">
+                <!-- Services -->
+                <div class="u-shadow-v28 g-bg-white text-left text-md-center rounded g-py-40">
+                    <!-- Services - Header -->
+                    <header class="text-center g-px-30 mb-5">
+                        <div class="g-mb-25">
+                    <span class="u-icon-v2 u-icon-size--md g-brd-text g-color-primary rounded-circle">
+                      <i class="icon-finance-211 u-line-icon-pro"></i>
+                    </span>
+                        </div>
+                        <h2 class="h5 g-font-weight-600">Marketing &amp; Consulting</h2>
+                        <p class="g-font-size-13">This is where we sit down, grab a cup of coffee and dial in the
+                            details.</p>
+                    </header>
+                    <!-- End Services - Header -->
+
+                    <!-- Services - List -->
+                    <ul class="list-unstyled mb-0">
+                        <li class="d-flex align-items-center g-px-35 g-py-15">
+                            <svg width="13" height="13" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1671 566q0 40-28 68l-724 724-136 136q-28 28-68 28t-68-28l-136-136-362-362q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 295 656-657q28-28 68-28t68 28l136 136q28 28 28 68z"
+                                      fill="#21a79b"/>
+                            </svg>
+                            <span class="g-color-text ml-3">24/7 support</span>
+                        </li>
+                        <li class="d-flex align-items-center u-shadow-v19 g-bg-secondary g-px-35 g-py-15">
+                            <svg width="13" height="13" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1671 566q0 40-28 68l-724 724-136 136q-28 28-68 28t-68-28l-136-136-362-362q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 295 656-657q28-28 68-28t68 28l136 136q28 28 28 68z"
+                                      fill="#21a79b"/>
+                            </svg>
+                            <span class="g-color-text ml-3">1610+ elements</span>
+                        </li>
+                        <li class="d-flex align-items-center g-px-35 g-py-15">
+                            <svg width="13" height="13" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1671 566q0 40-28 68l-724 724-136 136q-28 28-68 28t-68-28l-136-136-362-362q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 295 656-657q28-28 68-28t68 28l136 136q28 28 28 68z"
+                                      fill="#21a79b"/>
+                            </svg>
+                            <span class="g-color-text ml-3">400+ pages</span>
+                        </li>
+                        <li class="d-flex align-items-center u-shadow-v19 g-bg-secondary g-px-35 g-py-15">
+                            <svg width="13" height="13" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1671 566q0 40-28 68l-724 724-136 136q-28 28-68 28t-68-28l-136-136-362-362q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 295 656-657q28-28 68-28t68 28l136 136q28 28 28 68z"
+                                      fill="#21a79b"/>
+                            </svg>
+                            <span class="g-color-text ml-3">Unlimited domain or user</span>
+                        </li>
+                    </ul>
+                    <!-- End Services - List -->
+                </div>
+                <!-- End Services -->
+            </div>
+
+            <div class="col-md-6 col-lg-4 g-mb-50">
+                <!-- Services -->
+                <div class="u-shadow-v29 g-bg-primary text-left text-md-center rounded g-py-40">
+                    <!-- Services - Header -->
+                    <header class="text-center g-px-35 mb-5">
+                        <div class="g-mb-25">
+                    <span class="u-icon-v3 u-icon-size--md g-color-text g-bg-secondary rounded-circle">
+                      <i class="icon-finance-257 u-line-icon-pro"></i>
+                    </span>
+                        </div>
+                        <h2 class="h5 g-color-white g-font-weight-600">SEO &amp; Advertising</h2>
+                        <p class="g-color-white-opacity-0_9 g-font-size-13">Now that we've aligned the details, it's
+                            time to get things organized.</p>
+                    </header>
+                    <!-- End Services - Header -->
+
+                    <!-- Services - List -->
+                    <ul class="list-unstyled g-color-white-opacity-0_8 mb-0">
+                        <li class="d-flex align-items-center g-px-35 g-py-15">
+                            <svg width="13" height="13" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1671 566q0 40-28 68l-724 724-136 136q-28 28-68 28t-68-28l-136-136-362-362q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 295 656-657q28-28 68-28t68 28l136 136q28 28 28 68z"
+                                      fill="#fff"/>
+                            </svg>
+                            <span class="ml-3">24/7 support</span>
+                        </li>
+                        <li class="d-flex align-items-center u-shadow-v19 g-bg-white-opacity-0_1 g-px-35 g-py-15">
+                            <svg width="13" height="13" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1671 566q0 40-28 68l-724 724-136 136q-28 28-68 28t-68-28l-136-136-362-362q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 295 656-657q28-28 68-28t68 28l136 136q28 28 28 68z"
+                                      fill="#fff"/>
+                            </svg>
+                            <span class="ml-3">1610+ elements</span>
+                        </li>
+                        <li class="d-flex align-items-center g-px-35 g-py-15">
+                            <svg width="13" height="13" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1671 566q0 40-28 68l-724 724-136 136q-28 28-68 28t-68-28l-136-136-362-362q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 295 656-657q28-28 68-28t68 28l136 136q28 28 28 68z"
+                                      fill="#fff"/>
+                            </svg>
+                            <span class="ml-3">400+ pages</span>
+                        </li>
+                        <li class="d-flex align-items-center u-shadow-v19 g-bg-white-opacity-0_1 g-px-35 g-py-15">
+                            <svg width="13" height="13" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1671 566q0 40-28 68l-724 724-136 136q-28 28-68 28t-68-28l-136-136-362-362q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 295 656-657q28-28 68-28t68 28l136 136q28 28 28 68z"
+                                      fill="#fff"/>
+                            </svg>
+                            <span class="ml-3">Unlimited domain or user</span>
+                        </li>
+                    </ul>
+                    <!-- End Services - List -->
+                </div>
+                <!-- End Services -->
+            </div>
+
+            <div class="col-md-6 col-lg-4 g-mb-50">
+                <!-- Services -->
+                <div class="u-shadow-v28 g-bg-white text-left text-md-center rounded g-py-40">
+                    <!-- Services - Header -->
+                    <header class="text-center g-px-35 mb-5">
+                        <div class="g-mb-25">
+                    <span class="u-icon-v2 u-icon-size--md g-brd-text g-color-primary rounded-circle">
+                      <i class="icon-communication-164 u-line-icon-pro"></i>
+                    </span>
+                        </div>
+                        <h2 class="h5 g-font-weight-600">Design &amp; Development</h2>
+                        <p class="g-font-size-13">This is where we begin to visualize your sketches and make them
+                            into beautiful pixels.</p>
+                    </header>
+                    <!-- End Services - Header -->
+
+                    <!-- Services - List -->
+                    <ul class="list-unstyled mb-0">
+                        <li class="d-flex align-items-center g-px-35 g-py-15">
+                            <svg width="13" height="13" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1671 566q0 40-28 68l-724 724-136 136q-28 28-68 28t-68-28l-136-136-362-362q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 295 656-657q28-28 68-28t68 28l136 136q28 28 28 68z"
+                                      fill="#21a79b"/>
+                            </svg>
+                            <span class="g-color-text ml-3">24/7 support</span>
+                        </li>
+                        <li class="d-flex align-items-center u-shadow-v19 g-bg-secondary g-px-35 g-py-15">
+                            <svg width="13" height="13" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1671 566q0 40-28 68l-724 724-136 136q-28 28-68 28t-68-28l-136-136-362-362q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 295 656-657q28-28 68-28t68 28l136 136q28 28 28 68z"
+                                      fill="#21a79b"/>
+                            </svg>
+                            <span class="g-color-text ml-3">1610+ elements</span>
+                        </li>
+                        <li class="d-flex align-items-center g-px-35 g-py-15">
+                            <svg width="13" height="13" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1671 566q0 40-28 68l-724 724-136 136q-28 28-68 28t-68-28l-136-136-362-362q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 295 656-657q28-28 68-28t68 28l136 136q28 28 28 68z"
+                                      fill="#21a79b"/>
+                            </svg>
+                            <span class="g-color-text ml-3">400+ pages</span>
+                        </li>
+                        <li class="d-flex align-items-center u-shadow-v19 g-bg-secondary g-px-35 g-py-15">
+                            <svg width="13" height="13" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1671 566q0 40-28 68l-724 724-136 136q-28 28-68 28t-68-28l-136-136-362-362q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 295 656-657q28-28 68-28t68 28l136 136q28 28 28 68z"
+                                      fill="#21a79b"/>
+                            </svg>
+                            <span class="g-color-text ml-3">Unlimited domain or user</span>
+                        </li>
+                    </ul>
+                    <!-- End Services - List -->
+                </div>
+                <!-- End Services -->
+            </div>
+        </div>
+
+        <div class="text-center">
+            <a class="btn u-btn-outline-primary g-font-weight-600 g-font-size-12 g-rounded-30 g-py-15 g-px-35"
+               href="https://wrapbootstrap.com/theme/unify-responsive-website-template-WB0412697">Purchase Unify</a>
+        </div>
+    </div>
+</div>
+<!-- End Services -->
