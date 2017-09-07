afb8ffbc8c4fe2dc8141fdd50ef89ddc1d3d87d0
Ross Edlin <rossedlin@gmail.com>
2017-09-07 13:43:25 +0700

    add - nav

---
+++ resources/views/theme/unify-consulting/nav/default.blade.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ross Edlin
+ * Date: 07/09/2017
+ * Time: 12:57
+ */
+?>
+<!-- Header -->
+<header id="js-header" class="u-header u-header--sticky-top u-header--change-appearance u-header--change-logo"
+        data-header-fix-moment="0">
+    <div class="u-header__section g-bg-black-opacity-0_2 g-transition-0_3 g-py-7 g-py-23--md"
+         data-header-fix-moment-exclude="g-bg-black-opacity-0_5 g-py-23--md"
+         data-header-fix-moment-classes="u-shadow-v19 g-bg-white g-py-15--md">
+        <nav class="navbar navbar-toggleable-md g-py-0">
+            <div class="container">
+                <!-- Logo -->
+                <a href="/" class="navbar-brand u-header__logo">
+                    <img class="u-header__logo-img u-header__logo-img--main g-width-90"
+                         src="/theme/unify-consulting/img/logo.png" alt="Image description">
+                    <img class="u-header__logo-img g-width-90" src="/theme/unify-consulting/img/logo-dark.png"
+                         alt="Image description">
+                </a>
+                <!-- End Logo -->
+
+                <!-- Navigation -->
+                <div class="collapse navbar-collapse align-items-center flex-sm-row" id="navBar">
+                    <ul id="js-scroll-nav"
+                        class="navbar-nav text-uppercase g-letter-spacing-1 g-font-size-12 g-pt-20 g-pt-0--lg ml-auto">
+                        <li class="nav-item g-mr-15--lg g-mb-7 g-mb-0--lg">
+                            <a href="#home-section" class="nav-link p-0">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item g-mx-15--lg g-mb-7 g-mb-0--lg">
+                            <a href="#about-section" class="nav-link p-0">About</a>
+                        </li>
+                        <li class="nav-item g-mx-15--lg g-mb-7 g-mb-0--lg">
+                            <a href="#services-section" class="nav-link p-0">Services</a>
+                        </li>
+                        <li class="nav-item g-mx-15--lg g-mb-7 g-mb-0--lg">
+                            <a href="#news-section" class="nav-link p-0">News</a>
+                        </li>
+                        <li class="nav-item g-mx-15--lg g-mb-7 g-mb-0--lg">
+                            <a href="#portfolio-section" class="nav-link p-0">Portfolio</a>
+                        </li>
+                        <li class="nav-item g-mx-15--lg g-mb-7 g-mb-0--lg">
+                            <a href="#contact-section" class="nav-link p-0">Contact</a>
+                        </li>
+                        <li class="nav-item g-ml-15--lg">
+                            <a href="/" class="nav-link p-0">Main</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- End Navigation -->
+
+                <!-- Responsive Toggle Button -->
+                <button class="navbar-toggler btn g-line-height-1 g-brd-none g-pa-0 g-mt-12 ml-auto" type="button"
+                        aria-label="Toggle navigation"
+                        aria-expanded="false"
+                        aria-controls="navBar"
+                        data-toggle="collapse"
+                        data-target="#navBar">
+                <span class="hamburger hamburger--slider">
+                  <span class="hamburger-box">
+                    <span class="hamburger-inner"></span>
+                  </span>
+                </span>
+                </button>
+                <!-- End Responsive Toggle Button -->
+            </div>
+        </nav>
+    </div>
+</header>
+<!-- End Header -->
