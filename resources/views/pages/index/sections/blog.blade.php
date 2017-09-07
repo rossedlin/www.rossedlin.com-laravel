ddb981faf1d5fdf9bd54f2b325052ea524e15ffc
Ross Edlin <rossedlin@gmail.com>
2017-09-07 13:49:39 +0700

    add - blog

---
+++ resources/views/pages/index/sections/blog.blade.php
@@ -0,0 +1,166 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ross Edlin
+ * Date: 07/09/2017
+ * Time: 13:49
+ */
+?>
+<!-- News -->
+<div id="news-section" class="g-bg-secondary g-py-100">
+    <div class="container">
+        <!-- Heading -->
+        <div class="g-max-width-550 text-center mx-auto g-mb-70">
+            <h2 class="text-uppercase g-color-main-light-v1 g-font-weight-600 g-font-size-13 g-letter-spacing-2 mb-4">
+                News Blog</h2>
+            <h2 class="h3 mb-5">Read the latest news and blogs.</h2>
+        </div>
+        <!-- End Heading -->
+
+        <div class="row g-mx-minus-25 g-mb-50">
+            <div class="col-lg-6 g-px-25 g-mb-50">
+                <!-- Blog Grid Modern Blocks -->
+                <article class="row align-items-stretch no-gutters u-shadow-v29 g-transition-0_3">
+                    <div class="col-sm-6 g-bg-white g-rounded-left-5">
+                        <div class="g-pa-35">
+                            <ul class="list-inline g-color-gray-dark-v4 g-font-weight-600 g-font-size-12">
+                                <li class="list-inline-item mr-0">Alex Teseira</li>
+                                <li class="list-inline-item mx-2">&#183;</li>
+                                <li class="list-inline-item">5 June 2017</li>
+                            </ul>
+
+                            <h2 class="h5 g-color-black g-font-weight-600 mb-4">
+                                <a class="u-link-v5 g-color-black g-color-primary--hover g-cursor-pointer" href="#">Announcing
+                                    a free plan for small teams</a>
+                            </h2>
+                            <p class="g-color-gray-dark-v4 g-line-height-1_8 mb-4">At Wake, our mission has always
+                                been focused on bringing.</p>
+
+                            <ul class="list-inline g-font-size-12 mb-0">
+                                <li class="list-inline-item g-mb-10">
+                                    <a class="u-tags-v1 g-color-blue g-bg-blue-opacity-0_1 g-bg-blue--hover g-color-white--hover g-rounded-50 g-py-4 g-px-15"
+                                       href="#">Start-Up</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 g-bg-size-cover g-bg-pos-center g-min-height-300 g-rounded-right-5"
+                         data-bg-img-src="/theme/unify-consulting/img-temp/400x500/img1.jpg"></div>
+                </article>
+                <!-- End Blog Grid Modern Blocks -->
+            </div>
+
+            <div class="col-lg-6 g-px-25 g-mb-50">
+                <!-- Blog Grid Modern Blocks -->
+                <article class="row align-items-stretch no-gutters u-shadow-v29 g-transition-0_3">
+                    <div class="col-sm-6 g-bg-white g-rounded-left-5">
+                        <div class="g-pa-35">
+                            <ul class="list-inline g-color-gray-dark-v4 g-font-weight-600 g-font-size-12">
+                                <li class="list-inline-item mr-0">Alex Teseira</li>
+                                <li class="list-inline-item mx-2">&#183;</li>
+                                <li class="list-inline-item">5 June 2017</li>
+                            </ul>
+
+                            <h2 class="h5 g-color-black g-font-weight-600 mb-4">
+                                <a class="u-link-v5 g-color-black g-color-primary--hover g-cursor-pointer" href="#">Exclusive
+                                    interview with InVision's CEO</a>
+                            </h2>
+                            <p class="g-color-gray-dark-v4 g-line-height-1_8 mb-4">Clara Valberg is the founder and
+                                CEO of InVision.</p>
+
+                            <ul class="list-inline g-font-size-12 mb-0">
+                                <li class="list-inline-item g-mb-10">
+                                    <a class="u-tags-v1 g-color-brown g-bg-brown-opacity-0_1 g-bg-brown--hover g-color-white--hover g-rounded-50 g-py-4 g-px-15"
+                                       href="#">Start-Up</a>
+                                </li>
+                                <li class="list-inline-item g-mb-10">
+                                    <a class="u-tags-v1 g-color-pink g-bg-pink-opacity-0_1 g-bg-pink--hover g-color-white--hover g-rounded-50 g-py-4 g-px-15"
+                                       href="#">Interview</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 g-bg-size-cover g-bg-pos-center g-min-height-300 g-rounded-right-5"
+                         data-bg-img-src="/theme/unify-consulting/img-temp/400x500/img2.jpg"></div>
+                </article>
+                <!-- End Blog Grid Modern Blocks -->
+            </div>
+
+            <div class="w-100"></div>
+
+            <div class="col-lg-6 g-px-25 g-mb-50">
+                <!-- Blog Grid Modern Blocks -->
+                <article class="row align-items-stretch no-gutters u-shadow-v29 g-transition-0_3">
+                    <div class="col-sm-6 g-bg-white g-rounded-left-5">
+                        <div class="g-pa-35">
+                            <ul class="list-inline g-color-gray-dark-v4 g-font-weight-600 g-font-size-12">
+                                <li class="list-inline-item mr-0">Alex Teseira</li>
+                                <li class="list-inline-item mx-2">&#183;</li>
+                                <li class="list-inline-item">5 June 2017</li>
+                            </ul>
+
+                            <h2 class="h5 g-color-black g-font-weight-600 mb-4">
+                                <a class="u-link-v5 g-color-black g-color-primary--hover g-cursor-pointer" href="#">V’s
+                                    Barbershop Opening First South Florida Location</a>
+                            </h2>
+                            <p class="g-color-gray-dark-v4 g-line-height-1_8 mb-4">There’s nothing like a good
+                                barbershop.</p>
+
+                            <ul class="list-inline g-font-size-12 mb-0">
+                                <li class="list-inline-item g-mb-10">
+                                    <a class="u-tags-v1 g-color-bluegray g-bg-bluegray-opacity-0_1 g-bg-bluegray--hover g-color-white--hover g-rounded-50 g-py-4 g-px-15"
+                                       href="#">Barber</a>
+                                </li>
+                                <li class="list-inline-item g-mb-10">
+                                    <a class="u-tags-v1 g-color-primary g-bg-primary-opacity-0_1 g-bg-primary--hover g-color-white--hover g-rounded-50 g-py-4 g-px-15"
+                                       href="#">Style</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 g-bg-size-cover g-bg-pos-center g-min-height-300 g-rounded-right-5"
+                         data-bg-img-src="/theme/unify-consulting/img-temp/400x500/img3.jpg"></div>
+                </article>
+                <!-- End Blog Grid Modern Blocks -->
+            </div>
+
+            <div class="col-lg-6 g-px-25 g-mb-50">
+                <!-- Blog Grid Modern Blocks -->
+                <article class="row align-items-stretch no-gutters u-shadow-v29 g-transition-0_3">
+                    <div class="col-sm-6 g-bg-white g-rounded-left-5">
+                        <div class="g-pa-35">
+                            <ul class="list-inline g-color-gray-dark-v4 g-font-weight-600 g-font-size-12">
+                                <li class="list-inline-item mr-0">Alex Teseira</li>
+                                <li class="list-inline-item mx-2">&#183;</li>
+                                <li class="list-inline-item">5 June 2017</li>
+                            </ul>
+
+                            <h2 class="h5 g-color-black g-font-weight-600 mb-4">
+                                <a class="u-link-v5 g-color-black g-color-primary--hover g-cursor-pointer" href="#">The
+                                    Hypnotic Allure of Cinemagraphic Waves</a>
+                            </h2>
+                            <p class="g-color-gray-dark-v4 g-line-height-1_8 mb-4">Ocean waves are, by definition,
+                                in a constant state of motion.</p>
+
+                            <ul class="list-inline g-font-size-12 mb-0">
+                                <li class="list-inline-item g-mb-10">
+                                    <a class="u-tags-v1 g-color-brown g-bg-brown-opacity-0_1 g-bg-brown--hover g-color-white--hover g-rounded-50 g-py-4 g-px-15"
+                                       href="#">Animation</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 g-bg-size-cover g-bg-pos-center g-min-height-300 g-rounded-right-5"
+                         data-bg-img-src="/theme/unify-consulting/img-temp/400x500/img4.jpg"></div>
+                </article>
+                <!-- End Blog Grid Modern Blocks -->
+            </div>
+        </div>
+
+        <div class="text-center">
+            <a class="btn u-btn-outline-primary g-font-weight-600 g-font-size-12 g-rounded-30 g-py-15 g-px-35"
+               href="">Read more News</a>
+        </div>
+    </div>
+</div>
+<!-- End News -->
