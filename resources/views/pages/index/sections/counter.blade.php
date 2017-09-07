56dbfff9b03c2a2632408804105c66e327271faa
Ross Edlin <rossedlin@gmail.com>
2017-09-07 13:47:46 +0700

    add - counter

---
+++ resources/views/pages/index/sections/counter.blade.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ross Edlin
+ * Date: 07/09/2017
+ * Time: 13:47
+ */
+?>
+<!-- Counters -->
+<div class="dzsparallaxer auto-init height-is-based-on-content use-loading"
+     data-options='{direction: "reverse", settings_mode_oneelement_max_offset: "150"}'>
+    <div class="divimage dzsparallaxer--target w-100 g-bg-size-cover g-bg-img-hero g-bg-cover g-bg-black-opacity-0_7--after"
+         style="height: 130%; background-image: url(/theme/unify-consulting/img-temp/1920x500/img2.jpg);"></div>
+
+    <div class="container g-pt-100 g-pb-70">
+        <div class="row">
+            <div class="col-sm-6 col-lg-3 align-items-end mt-auto g-mb-50">
+                <div class="text-center">
+                    <span class="js-counter g-color-primary g-font-weight-600 g-font-size-60 g-line-height-0_9 mr-2">75</span>
+                    <h3 class="d-inline-block g-color-secondary g-font-weight-600 g-font-size-22 mb-0">Clients</h3>
+                </div>
+            </div>
+
+            <div class="col-sm-6 col-lg-3 align-items-end mt-auto g-mb-50">
+                <div class="text-center">
+                    <span class="js-counter g-color-primary g-font-weight-600 g-font-size-60 g-line-height-0_9 mr-2">20</span>
+                    <h3 class="d-inline-block g-color-secondary g-font-weight-600 g-font-size-22 mb-0">Products</h3>
+                </div>
+            </div>
+
+            <div class="col-sm-6 col-lg-3 align-items-end mt-auto g-mb-50">
+                <div class="text-center">
+                    <span class="js-counter g-color-primary g-font-weight-600 g-font-size-60 g-line-height-0_9 mr-2">15</span>
+                    <h3 class="d-inline-block g-color-secondary g-font-weight-600 g-font-size-22 mb-0">Members</h3>
+                </div>
+            </div>
+
+            <div class="col-sm-6 col-lg-3 align-items-end mt-auto g-mb-50">
+                <div class="text-center">
+                    <span class="js-counter g-color-primary g-font-weight-600 g-font-size-60 g-line-height-0_9 mr-2">50</span>
+                    <h3 class="d-inline-block g-color-secondary g-font-weight-600 g-font-size-22 mb-0">Projects</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Counters -->
