53491bce942d9c3a4936d56b683f9e7e02a4eecb
Ross Edlin <rossedlin@gmail.com>
2017-09-13 16:59:03 +0700

    improve styling of contact page

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * @author Ross Edlin <contact@rossedlin.com>
+ *
+ * Date: 13/09/2017
+ * Time: 16:52
+ */
+?>
+@extends('theme.unify-consulting.layouts.default')
+
+@section('content')
+
+    @include('pages.contact.sections.contact')
+
+@stop
+
+@section('body.post')
+    @include('pages.index.snippets.body-post')
+@stop
