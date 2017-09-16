72d7b1a0d2c255cb8b670d014a79621109b73e85
Ross Edlin <rossedlin@gmail.com>
2017-09-16 23:12:12 +0700

    add - theme to test page

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -6,23 +6,37 @@
  *
  * Date: 16/09/2017
  * Time: 19:47
+ *
+ * @var mixed $dump
  */
 ?>
 @extends('unify-consulting.layout')
 
 @section('content')
 
-    <main class="g-min-height-100vh g-flex-centered g-bg-size-cover g-bg-cover g-bg-bluegray-opacity-0_3--after g-pa-15"
-          style="background-image: url(/img/pixabay/landscape-2389023-1920x960.jpg);">
-        <div class="text-center g-max-width-700 g-flex-centered-item g-z-index-1 g-color-white">
-            <h1 class="display-3 g-mb-30 text-shadow-1" style="font-size: 88px;color: #e6e6e6">Coming Soon</h1>
-            <p class="g-font-size-22 g-mb-50 text-shadow-1" style="font-size: 24px; color: #e6e6e6;">In the mean time,
-                why not <a href="#contact">contact me!</a></p>
-        </div>
-    </main>
+    <!-- Web Scraping -->
+    <div id="test">
+        <div class="container demo g-pt-100">
 
-    @include('sections.quote.one')
-    @include('sections.contact')
+            <!-- Heading -->
+            <div class="text-center mx-auto" style="margin-top: 60px;">
+                <h1 class="text-uppercase g-color-main-light-v1 g-font-weight-600 g-letter-spacing-2 mb-4"
+                    style="font-size: 32px;">
+                    Test
+                </h1>
+            </div>
+            <!-- End Heading -->
+
+            <hr/>
+
+            <!-- Web Scraping Form -->
+            <section class="container mx-auto">
+                <?php pre($dump) ?>
+            </section>
+            <!-- End Web Scraping Form -->
+        </div>
+    </div>
+    <!-- End Web Scraping -->
 
 @stop
 
