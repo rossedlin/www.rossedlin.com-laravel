c57b57c9fc0adfc0dd9dbfa0e39b49fce9c6b86a
Ross Edlin <rossedlin@gmail.com>
2017-09-18 18:12:52 +0700

    add - success / danger parts

---
+++ resources/parts/alerts/danger-1.blade.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * @author Ross Edlin <contact@rossedlin.com>
+ *
+ * Date: 18/09/2017
+ * Time: 18:01
+ *
+ * @var string $title
+ * @var string $message
+ */
+?>
+<div class="alert alert-danger alert-dismissible fade show" role="alert">
+
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+
+    <h4 class="alert-heading"><?= $title ?></h4>
+    <p><?= $message ?></p>
+</div>
