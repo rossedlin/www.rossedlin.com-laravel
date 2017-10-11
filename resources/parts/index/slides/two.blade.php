6e0a4560fa476eb6c59f389dfe1e9a5d45bd8cb4
Ross Edlin <rossedlin@gmail.com>
2017-10-11 16:20:29 +0700

    update slide buttons

--- resources/parts/index/slides/two.blade.php
+++ resources/parts/index/slides/two.blade.php
@@ -45,7 +45,7 @@
     <div class="tp-caption   tp-resizeme rs-parallaxlevel-4"
          id="slide-2801-layer-2"
          data-x="['center','center','center','center']" data-hoffset="['-10','-10','-10','-10']"
-         data-y="['middle','middle','middle','middle']" data-voffset="['-100','-100','-100','-100']"
+         data-y="['middle','middle','middle','middle']" data-voffset="['-80','-80','-80','-80']"
          data-fontsize="['27','27','25','30']"
          data-lineheight="['40','40','35','40']"
          data-width="none"
@@ -63,55 +63,55 @@
          data-paddingleft="[0,0,0,0]"
 
          style="z-index: 8; white-space: nowrap; font-weight: 400; color: rgba(255, 255, 255, 1.00);border-width:0px;">
-        When you stand on the edge, staring into endless mountains <br />
+        When you stand on the edge, staring into endless mountains <br/>
         You'll realise just how much this world has to offer.
     </div>
 
     <!-- LAYER NR. 5 -->
-    <div class="tp-caption rev-btn  tp-resizeme rs-parallaxlevel-4"
-         id="slide-2801-layer-4"
-         data-x="['right','right','center','center']" data-hoffset="['630','525','-105','100']"
-         data-y="['middle','middle','middle','middle']" data-voffset="['240','240','240','240']"
-         data-width="none"
-         data-height="none"
-         data-whitespace="nowrap"
+    <a class="tp-caption rev-btn  tp-resizeme rs-parallaxlevel-4"
+       href="/about"
+       id="slide-2801-layer-4"
+       data-x="['right','right','center','center']" data-hoffset="['630','525','-105','100']"
+       data-y="['middle','middle','middle','middle']" data-voffset="['240','240','240','240']"
+       data-width="none"
+       data-height="none"
+       data-whitespace="nowrap"
 
-         data-type="button"
-         data-actions='[{"event":"click","action":"scrollbelow","offset":"px","delay":""}]'
-         data-responsive_offset="on"
+       data-type="button"
+       data-responsive_offset="on"
 
-         data-frames='[{"from":"x:-50px;opacity:0;","speed":2500,"to":"o:1;","delay":500,"ease":"Power4.easeOut"},{"delay":"wait","speed":300,"to":"opacity:0;","ease":"nothing"},{"frame":"hover","speed":"150","ease":"Power2.easeInOut","to":"o:1;rX:0;rY:0;rZ:0;z:0;","style":"c:rgba(0, 0, 0, 1.00);bg:rgba(255, 255, 255, 1.00);bc:rgba(255, 255, 255, 0);bw:2px 2px 2px 2px;"}]'
-         data-textAlign="['left','left','left','left']"
-         data-paddingtop="[0,0,0,0]"
-         data-paddingright="[50,50,50,50]"
-         data-paddingbottom="[0,0,0,0]"
-         data-paddingleft="[50,50,50,50]"
+       data-frames='[{"from":"x:-50px;opacity:0;","speed":2500,"to":"o:1;","delay":500,"ease":"Power4.easeOut"},{"delay":"wait","speed":300,"to":"opacity:0;","ease":"nothing"},{"frame":"hover","speed":"150","ease":"Power2.easeInOut","to":"o:1;rX:0;rY:0;rZ:0;z:0;","style":"c:rgba(0, 0, 0, 1.00);bg:rgba(255, 255, 255, 1.00);bc:rgba(255, 255, 255, 0);bw:2px 2px 2px 2px;"}]'
+       data-textAlign="['left','left','left','left']"
+       data-paddingtop="[0,0,0,0]"
+       data-paddingright="[50,50,50,50]"
+       data-paddingbottom="[0,0,0,0]"
+       data-paddingleft="[50,50,50,50]"
 
-         style="z-index: 9; white-space: nowrap; font-size: 15px; line-height: 46px; color: rgba(255, 255, 255, 1.00);background-color:rgba(0, 0, 0, 0);border-color:rgba(255, 255, 255, 1);border-style:solid;border-width:2px;border-radius:30px;outline:none;box-shadow:none;box-sizing:border-box;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;cursor:pointer;">
-        Learn More
-    </div>
+       style="z-index: 9; white-space: nowrap; font-size: 15px; line-height: 46px; color: rgba(255, 255, 255, 1.00);background-color:rgba(0, 0, 0, 0);border-color:rgba(255, 255, 255, 1);border-style:solid;border-width:2px;border-radius:30px;outline:none;box-shadow:none;box-sizing:border-box;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;cursor:pointer;">
+        About Me
+    </a>
 
     <!-- LAYER NR. 7 -->
-    <div class="tp-caption rev-btn  tp-resizeme rs-parallaxlevel-4"
-         href="#portfolio-section" target="_self" id="slide-2801-layer-5"
-         data-x="['left','left','center','center']" data-hoffset="['630','525','105','100']"
-         data-y="['middle','middle','middle','middle']" data-voffset="['240','240','240','240']"
-         data-width="none"
-         data-height="none"
-         data-whitespace="nowrap"
+    <a class="tp-caption rev-btn  tp-resizeme rs-parallaxlevel-4"
+       href="/portfolio"
+       id="slide-2801-layer-5"
+       data-x="['left','left','center','center']" data-hoffset="['630','525','105','100']"
+       data-y="['middle','middle','middle','middle']" data-voffset="['240','240','240','240']"
+       data-width="none"
+       data-height="none"
+       data-whitespace="nowrap"
 
-         data-type="button"
-         data-actions='[{"event":"click","action":"scrollbelow","offset":"px","delay":""}]'
-         data-responsive_offset="on"
+       data-type="button"
+       data-responsive_offset="on"
 
-         data-frames='[{"from":"x:50px;opacity:0;","speed":2500,"to":"o:1;","delay":500,"ease":"Power4.easeOut"},{"delay":"wait","speed":300,"to":"opacity:0;","ease":"nothing"},{"frame":"hover","speed":"150","ease":"Power2.easeInOut","to":"o:1;rX:0;rY:0;rZ:0;z:0;","style":"c:rgba(0, 0, 0, 1.00);bg:rgba(255, 255, 255, 1.00);bc:rgba(255, 255, 255, 0);bw:2px 2px 2px 2px;"}]'
-         data-textAlign="['left','left','left','left']"
-         data-paddingtop="[0,0,0,0]"
-         data-paddingright="[50,50,50,50]"
-         data-paddingbottom="[0,0,0,0]"
-         data-paddingleft="[50,50,50,50]"
+       data-frames='[{"from":"x:50px;opacity:0;","speed":2500,"to":"o:1;","delay":500,"ease":"Power4.easeOut"},{"delay":"wait","speed":300,"to":"opacity:0;","ease":"nothing"},{"frame":"hover","speed":"150","ease":"Power2.easeInOut","to":"o:1;rX:0;rY:0;rZ:0;z:0;","style":"c:rgba(0, 0, 0, 1.00);bg:rgba(255, 255, 255, 1.00);bc:rgba(255, 255, 255, 0);bw:2px 2px 2px 2px;"}]'
+       data-textAlign="['left','left','left','left']"
+       data-paddingtop="[0,0,0,0]"
+       data-paddingright="[50,50,50,50]"
+       data-paddingbottom="[0,0,0,0]"
+       data-paddingleft="[50,50,50,50]"
 
-         style="z-index: 9; white-space: nowrap; font-size: 15px; line-height: 46px; color: rgba(255, 255, 255, 1.00);background-color:rgba(0, 0, 0, 0);border-color:rgba(255, 255, 255, 1);border-style:solid;border-width:2px;border-radius:30px;outline:none;box-shadow:none;box-sizing:border-box;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;cursor:pointer;">
-        Our Work
-    </div>
+       style="z-index: 9; white-space: nowrap; font-size: 15px; line-height: 46px; color: rgba(255, 255, 255, 1.00);background-color:rgba(0, 0, 0, 0);border-color:rgba(255, 255, 255, 1);border-style:solid;border-width:2px;border-radius:30px;outline:none;box-shadow:none;box-sizing:border-box;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;cursor:pointer;">
+        My Portfolio
+    </a>
 </li>
