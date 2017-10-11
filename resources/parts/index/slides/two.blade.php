9228b5926d35fe69c8184092c34e046f2b3dea16
Ross Edlin <rossedlin@gmail.com>
2017-10-11 13:41:15 +0700

    add - index / slide / two

---
+++ resources/parts/index/slides/two.blade.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * @author Ross Edlin <contact@rossedlin.com>
+ *
+ * Date: 11/10/2017
+ * Time: 13:17
+ */
+?>
+<li data-index="rs-2801" data-transition="slideup" data-slotamount="default" data-hideafterloop="0"
+    data-hideslideonmobile="off" data-easein="default" data-easeout="default"
+    data-masterspeed="default" data-thumb="/theme/unify-consulting/img-temp/1920x1080/img2.jpg"
+    data-rotate="0" data-saveperformance="off" data-title="Slide" data-param1="" data-param2=""
+    data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8=""
+    data-param9="" data-param10="" data-description="">
+    <!-- MAIN IMAGE -->
+    <img src="/img/thailand/pai-canyon.jpg" alt="" data-bgposition="center center"
+         data-bgfit="cover" class="rev-slidebg">
+    <!-- LAYERS -->
+
+    <!-- LAYER NR. 1 -->
+    <div class="tp-caption tp-shape tp-shapewrapper "
+         id="slide-2801-layer-7"
+         data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
+         data-y="['middle','middle','middle','middle']" data-voffset="['0','0','0','0']"
+         data-width="full"
+         data-height="full"
+         data-whitespace="nowrap"
+
+         data-type="shape"
+         data-basealign="slide"
+         data-responsive_offset="off"
+         data-responsive="off"
+         data-frames='[{"from":"opacity:0;","speed":500,"to":"o:1;","delay":0,"ease":"Power4.easeOut"},{"speed":5000,"to":"opacity:0;","ease":"Power4.easeInOut"}]'
+         data-textAlign="['left','left','left','left']"
+         data-paddingtop="[0,0,0,0]"
+         data-paddingright="[0,0,0,0]"
+         data-paddingbottom="[0,0,0,0]"
+         data-paddingleft="[0,0,0,0]"
+
+         style="z-index: 5;background-color:rgba(0, 0, 0, 0.3);border-color:rgba(0, 0, 0, 0);border-width:0px;"></div>
+
+    <!-- LAYER NR. 4 -->
+    <div class="tp-caption   tp-resizeme rs-parallaxlevel-4"
+         id="slide-2801-layer-2"
+         data-x="['center','center','center','center']" data-hoffset="['-10','-10','-10','-10']"
+         data-y="['middle','middle','middle','middle']" data-voffset="['-100','-100','-100','-100']"
+         data-fontsize="['27','27','25','30']"
+         data-lineheight="['40','40','35','40']"
+         data-width="none"
+         data-height="none"
+         data-whitespace="nowrap"
+
+         data-type="text"
+         data-responsive_offset="on"
+
+         data-frames='[{"from":"y:50px;sX:1;sY:1;opacity:0;","speed":2500,"to":"o:1;","delay":500,"ease":"Power4.easeOut"},{"delay":"wait","speed":300,"to":"opacity:0;","ease":"nothing"}]'
+         data-textAlign="['center','center','center','center']"
+         data-paddingtop="[0,0,0,0]"
+         data-paddingright="[0,0,0,0]"
+         data-paddingbottom="[0,0,0,0]"
+         data-paddingleft="[0,0,0,0]"
+
+         style="z-index: 8; white-space: nowrap; font-weight: 400; color: rgba(255, 255, 255, 1.00);border-width:0px;">
+        When you stand on the edge, staring into endless mountains <br />
+        You'll realise just how much this world has to offer.
+    </div>
+
+    <!-- LAYER NR. 5 -->
+    <div class="tp-caption rev-btn  tp-resizeme rs-parallaxlevel-4"
+         id="slide-2801-layer-4"
+         data-x="['right','right','center','center']" data-hoffset="['630','525','-105','100']"
+         data-y="['middle','middle','middle','middle']" data-voffset="['240','240','240','240']"
+         data-width="none"
+         data-height="none"
+         data-whitespace="nowrap"
+
+         data-type="button"
+         data-actions='[{"event":"click","action":"scrollbelow","offset":"px","delay":""}]'
+         data-responsive_offset="on"
+
+         data-frames='[{"from":"x:-50px;opacity:0;","speed":2500,"to":"o:1;","delay":500,"ease":"Power4.easeOut"},{"delay":"wait","speed":300,"to":"opacity:0;","ease":"nothing"},{"frame":"hover","speed":"150","ease":"Power2.easeInOut","to":"o:1;rX:0;rY:0;rZ:0;z:0;","style":"c:rgba(0, 0, 0, 1.00);bg:rgba(255, 255, 255, 1.00);bc:rgba(255, 255, 255, 0);bw:2px 2px 2px 2px;"}]'
+         data-textAlign="['left','left','left','left']"
+         data-paddingtop="[0,0,0,0]"
+         data-paddingright="[50,50,50,50]"
+         data-paddingbottom="[0,0,0,0]"
+         data-paddingleft="[50,50,50,50]"
+
+         style="z-index: 9; white-space: nowrap; font-size: 15px; line-height: 46px; color: rgba(255, 255, 255, 1.00);background-color:rgba(0, 0, 0, 0);border-color:rgba(255, 255, 255, 1);border-style:solid;border-width:2px;border-radius:30px;outline:none;box-shadow:none;box-sizing:border-box;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;cursor:pointer;">
+        Learn More
+    </div>
+
+    <!-- LAYER NR. 7 -->
+    <div class="tp-caption rev-btn  tp-resizeme rs-parallaxlevel-4"
+         href="#portfolio-section" target="_self" id="slide-2801-layer-5"
+         data-x="['left','left','center','center']" data-hoffset="['630','525','105','100']"
+         data-y="['middle','middle','middle','middle']" data-voffset="['240','240','240','240']"
+         data-width="none"
+         data-height="none"
+         data-whitespace="nowrap"
+
+         data-type="button"
+         data-actions='[{"event":"click","action":"scrollbelow","offset":"px","delay":""}]'
+         data-responsive_offset="on"
+
+         data-frames='[{"from":"x:50px;opacity:0;","speed":2500,"to":"o:1;","delay":500,"ease":"Power4.easeOut"},{"delay":"wait","speed":300,"to":"opacity:0;","ease":"nothing"},{"frame":"hover","speed":"150","ease":"Power2.easeInOut","to":"o:1;rX:0;rY:0;rZ:0;z:0;","style":"c:rgba(0, 0, 0, 1.00);bg:rgba(255, 255, 255, 1.00);bc:rgba(255, 255, 255, 0);bw:2px 2px 2px 2px;"}]'
+         data-textAlign="['left','left','left','left']"
+         data-paddingtop="[0,0,0,0]"
+         data-paddingright="[50,50,50,50]"
+         data-paddingbottom="[0,0,0,0]"
+         data-paddingleft="[50,50,50,50]"
+
+         style="z-index: 9; white-space: nowrap; font-size: 15px; line-height: 46px; color: rgba(255, 255, 255, 1.00);background-color:rgba(0, 0, 0, 0);border-color:rgba(255, 255, 255, 1);border-style:solid;border-width:2px;border-radius:30px;outline:none;box-shadow:none;box-sizing:border-box;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;cursor:pointer;">
+        Our Work
+    </div>
+</li>
