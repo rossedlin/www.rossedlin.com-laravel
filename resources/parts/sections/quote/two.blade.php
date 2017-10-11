c81a0d7149141a8d6ed79b07154762bd51a5a8b8
Ross Edlin <rossedlin@gmail.com>
2017-10-11 09:31:14 +0700

    add - quote two

---
+++ resources/parts/sections/quote/two.blade.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ross Edlin
+ * Date: 07/09/2017
+ * Time: 13:16
+ */
+?>
+<!-- Quote -->
+<div class="dzsparallaxer auto-init height-is-based-on-content use-loading"
+     data-options='{direction: "reverse", settings_mode_oneelement_max_offset: "150"}'>
+    <div class="divimage dzsparallaxer--target w-100 g-bg-size-cover g-bg-img-hero g-bg-cover g-bg-black-opacity-0_6--after"
+         style="height: 130%; background-image: url(/img/pixabay/mobile-phone-1419275-1920x600.jpg);"></div>
+
+    <div class="container text-center g-pos-rel g-z-index-1 g-py-100">
+        <blockquote
+                class="g-color-white g-font-family-merriweather g-font-size-30 g-font-size-40--md g-line-height-1_4 g-mb-50">
+            <span class="g-color-primary g-font-family-ptsans g-font-size-90 g-line-height-0_7 g-pos-rel g-top-30">&#8220;</span>
+            We have two lives, and the second begins when we realize we only have one.
+            <span class="g-color-primary g-font-family-ptsans g-font-size-90 g-line-height-0_7 g-pos-rel g-top-30">&#8221;</span>
+        </blockquote>
+        <h4 class="g-color-white g-font-size-15 text-uppercase mb-1"><span
+                    class="d-inline-block align-middle g-width-40 g-height-2 g-bg-primary mr-3"></span> Confucius
+        </h4>
+    </div>
+</div>
+<!-- End Quote -->
