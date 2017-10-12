5fc4f1622ae70206ea433cc53c631a4c53a5a32e
Ross Edlin <rossedlin@gmail.com>
2017-10-12 16:55:51 +0700

    add - initial social page

---
+++ resources/parts/sections/about/social.blade.php
@@ -0,0 +1,191 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * @author Ross Edlin <contact@rossedlin.com>
+ *
+ * Date: 12/10/2017
+ * Time: 14:18
+ */
+?>
+<!-- User Accounts -->
+<div class="row">
+    <!-- User Accounts (Option 1) -->
+    <div class="col-md-6">
+        <div class="card border-0 rounded-0">
+            <!-- Panel Header -->
+            <div class="card-header d-flex align-items-center justify-content-between g-bg-gray-light-v5 border-0 g-mb-15">
+                <h3 class="h6 mb-0">
+                    <i class="icon-directions g-pos-rel g-top-1 g-mr-5"></i> User Accounts
+                    <small>(option 1)</small>
+                </h3>
+                <div class="dropdown g-mb-10 g-mb-0--md">
+                      <span class="d-block g-color-primary--hover g-cursor-pointer g-mr-minus-5 g-pa-5"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          <i class="icon-options-vertical g-pos-rel g-top-1"></i>
+                        </span>
+                    <div class="dropdown-menu dropdown-menu-right rounded-0 g-mt-10">
+                        <a class="dropdown-item g-px-10" href="#">
+                            <i class="icon-layers g-font-size-12 g-color-gray-dark-v5 g-mr-5"></i> Projects
+                        </a>
+                        <a class="dropdown-item g-px-10" href="#">
+                            <i class="icon-wallet g-font-size-12 g-color-gray-dark-v5 g-mr-5"></i> Wallets
+                        </a>
+                        <a class="dropdown-item g-px-10" href="#">
+                            <i class="icon-fire g-font-size-12 g-color-gray-dark-v5 g-mr-5"></i> Reports
+                        </a>
+                        <a class="dropdown-item g-px-10" href="#">
+                            <i class="icon-settings g-font-size-12 g-color-gray-dark-v5 g-mr-5"></i> Users Setting
+                        </a>
+
+                        <div class="dropdown-divider"></div>
+
+                        <a class="dropdown-item g-px-10" href="#">
+                            <i class="icon-plus g-font-size-12 g-color-gray-dark-v5 g-mr-5"></i> View More
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!-- End Panel Header -->
+
+            <!-- Panel Body -->
+            <div class="js-scrollbar card-block u-info-v1-1 g-bg-white-gradient-v1--after g-height-300 g-pa-0">
+                <ul class="list-unstyled">
+                    <li class="d-flex g-brd-bottom g-brd-gray-light-v4 g-py-10 g-px-15">
+                        <a class="u-icon-v3 u-icon-size--xs g-bg-facebook g-color-white rounded-circle g-mr-15"
+                           href="#"><i class="fa fa-facebook"></i></a>
+                        <a class="g-color-facebook" href="#">77johndoe</a>
+                    </li>
+                    <li class="d-flex g-brd-bottom g-brd-gray-light-v4 g-py-10 g-px-15">
+                        <a class="u-icon-v3 u-icon-size--xs g-bg-twitter g-color-white rounded-circle g-mr-15"
+                           href="#"><i class="fa fa-twitter"></i></a>
+                        <a class="g-color-twitter" href="#">john-doe1977</a>
+                    </li>
+                    <li class="d-flex g-brd-bottom g-brd-gray-light-v4 g-py-10 g-px-15">
+                        <a class="u-icon-v3 u-icon-size--xs g-bg-darkblue g-color-white rounded-circle g-mr-15"
+                           href="#"><i class="fa fa-paypal"></i></a>
+                        <a class="g-color-darkblue g-color-darkblue--hover" href="#">john.doe@htmlstream.com</a>
+                    </li>
+                    <li class="d-flex g-brd-bottom g-brd-gray-light-v4 g-py-10 g-px-15">
+                        <a class="u-icon-v3 u-icon-size--xs g-bg-lightred g-color-white rounded-circle g-mr-15"
+                           href="#"><i class="fa fa-google-plus"></i></a>
+                        <a class="g-color-lightred g-color-lightred--hover" href="#">john_doe</a>
+                    </li>
+                    <li class="d-flex g-brd-bottom g-brd-gray-light-v4 g-py-10 g-px-15">
+                        <a class="u-icon-v3 u-icon-size--xs g-bg-black g-color-white rounded-circle g-mr-15"
+                           href="#"><i class="fa fa-github"></i></a>
+                        <a class="g-color-black g-color-black--hover" href="#">john.doe.github</a>
+                    </li>
+                    <li class="d-flex g-brd-bottom g-brd-gray-light-v4 g-py-10 g-px-15">
+                        <a class="u-icon-v3 u-icon-size--xs g-bg-skype g-color-white rounded-circle g-mr-15"
+                           href="#"><i class="fa fa-skype"></i></a>
+                        <a class="g-color-skype" href="#">john_doe_723</a>
+                    </li>
+                    <li class="d-flex g-brd-bottom g-brd-gray-light-v4 g-py-10 g-px-15">
+                        <a class="u-icon-v3 u-icon-size--xs g-bg-linkedin g-color-white rounded-circle g-mr-15"
+                           href="#"><i class="fa fa-linkedin"></i></a>
+                        <a class="g-color-linkedin" href="#">john.doe77</a>
+                    </li>
+                    <li class="d-flex g-brd-bottom g-brd-gray-light-v4 g-py-10 g-px-15">
+                        <a class="u-icon-v3 u-icon-size--xs g-bg-lightred g-color-white rounded-circle g-mr-15"
+                           href="#"><i class="fa fa-envelope"></i></a>
+                        <a class="g-color-lightred g-color-lightred--hover" href="#">john.doe@htmlstream.com</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- End Panel Body -->
+        </div>
+    </div>
+    <!-- End User Accounts (Option 1) -->
+
+    <!-- User Accounts (Option 2) -->
+    <div class="col-md-6">
+        <!-- User Accounts -->
+        <div class="card border-0 rounded-0">
+            <!-- Panel Header -->
+            <div class="card-header d-flex align-items-center justify-content-between g-bg-gray-light-v5 border-0 g-mb-15">
+                <h3 class="h6 mb-0">
+                    <i class="icon-directions g-pos-rel g-top-1 g-mr-5"></i> User Accounts
+                    <small>(option 2)</small>
+                </h3>
+                <div class="dropdown g-mb-10 g-mb-0--md">
+                      <span class="d-block g-color-primary--hover g-cursor-pointer g-mr-minus-5 g-pa-5"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          <i class="icon-options-vertical g-pos-rel g-top-1"></i>
+                        </span>
+                    <div class="dropdown-menu dropdown-menu-right rounded-0 g-mt-10">
+                        <a class="dropdown-item g-px-10" href="#">
+                            <i class="icon-layers g-font-size-12 g-color-gray-dark-v5 g-mr-5"></i> Projects
+                        </a>
+                        <a class="dropdown-item g-px-10" href="#">
+                            <i class="icon-wallet g-font-size-12 g-color-gray-dark-v5 g-mr-5"></i> Wallets
+                        </a>
+                        <a class="dropdown-item g-px-10" href="#">
+                            <i class="icon-fire g-font-size-12 g-color-gray-dark-v5 g-mr-5"></i> Reports
+                        </a>
+                        <a class="dropdown-item g-px-10" href="#">
+                            <i class="icon-settings g-font-size-12 g-color-gray-dark-v5 g-mr-5"></i> Users Setting
+                        </a>
+
+                        <div class="dropdown-divider"></div>
+
+                        <a class="dropdown-item g-px-10" href="#">
+                            <i class="icon-plus g-font-size-12 g-color-gray-dark-v5 g-mr-5"></i> View More
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!-- End Panel Header -->
+
+            <!-- Panel Body -->
+            <div class="js-scrollbar card-block u-info-v1-1 g-bg-white-gradient-v1--after g-height-300 g-pa-0">
+                <ul class="list-unstyled">
+                    <li class="d-flex justify-content-between g-brd-bottom g-brd-gray-light-v4 g-py-10 g-px-15">
+                            <span class="text-nowrap g-mr-10"><i
+                                        class="fa fa-facebook g-color-facebook g-pos-rel g-top-1 g-mr-10"></i> Facebook</span>
+                        <a class="g-color-facebook" href="#">77johndoe</a>
+                    </li>
+                    <li class="d-flex justify-content-between g-brd-bottom g-brd-gray-light-v4 g-py-10 g-px-15">
+                            <span class="text-nowrap g-mr-10"><i
+                                        class="fa fa-twitter g-color-twitter g-pos-rel g-top-1 g-mr-10"></i> Twitter</span>
+                        <a class="g-color-twitter" href="#">john-doe1977</a>
+                    </li>
+                    <li class="d-flex justify-content-between g-brd-bottom g-brd-gray-light-v4 g-py-10 g-px-15">
+                            <span class="text-nowrap g-mr-10"><i
+                                        class="fa fa-paypal g-color-darkblue g-pos-rel g-top-1 g-mr-10"></i> PayPal</span>
+                        <a class="g-color-darkblue g-color-darkblue--hover" href="#">john.doe@htmlstream.com</a>
+                    </li>
+                    <li class="d-flex justify-content-between g-brd-bottom g-brd-gray-light-v4 g-py-10 g-px-15">
+                            <span class="text-nowrap g-mr-10"><i
+                                        class="fa fa-google g-color-lightred g-pos-rel g-top-1 g-mr-10"></i> Google Plus</span>
+                        <a class="g-color-lightred g-color-lightred--hover" href="#">john_doe</a>
+                    </li>
+                    <li class="d-flex justify-content-between g-brd-bottom g-brd-gray-light-v4 g-py-10 g-px-15">
+                            <span class="text-nowrap g-mr-10"><i
+                                        class="fa fa-github g-color-black g-pos-rel g-top-1 g-mr-10"></i> Github</span>
+                        <a class="g-color-black g-color-black--hover" href="#">john.doe.github</a>
+                    </li>
+                    <li class="d-flex justify-content-between g-brd-bottom g-brd-gray-light-v4 g-py-10 g-px-15">
+                            <span class="text-nowrap g-mr-10"><i
+                                        class="fa fa-skype g-color-skype g-pos-rel g-top-1 g-mr-10"></i> Skype</span>
+                        <a class="g-color-skype" href="#">john_doe_723</a>
+                    </li>
+                    <li class="d-flex justify-content-between g-brd-bottom g-brd-gray-light-v4 g-py-10 g-px-15">
+                            <span class="text-nowrap g-mr-10"><i
+                                        class="fa fa-linkedin g-color-linkedin g-pos-rel g-top-1 g-mr-10"></i> Linkedin</span>
+                        <a class="g-color-linkedin" href="#">john.doe77</a>
+                    </li>
+                    <li class="d-flex justify-content-between g-brd-bottom g-brd-gray-light-v4 g-py-10 g-px-15">
+                            <span class="text-nowrap g-mr-10"><i
+                                        class="fa fa-envelope g-color-lightred g-pos-rel g-top-1 g-mr-10"></i> Gmail</span>
+                        <a class="g-color-lightred g-color-lightred--hover" href="#">john.doe@htmlstream.com</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- End Panel Body -->
+        </div>
+        <!-- End User Accounts -->
+    </div>
+    <!-- End User Accounts (Option 2) -->
+</div>
+<!-- End User Accounts -->
