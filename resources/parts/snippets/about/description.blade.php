a0acd5d13bd1607366019f42b1184a045d1d257c
Ross Edlin <rossedlin@gmail.com>
2017-10-12 14:58:02 +0700

    add about / content / desc

---
+++ resources/parts/snippets/about/description.blade.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * @author Ross Edlin <contact@rossedlin.com>
+ *
+ * Date: 12/10/2017
+ * Time: 14:16
+ */
+?>
+<p>
+    Problem solving and Travelling, my two favourite hobbies. <br/>
+    I absolutely love solving peoples problems, it's probably why I got into software
+    development! <br/>
+    The feeling of fixing something, helping someone or reliving someone of their pain gives
+    me immense satisfaction. <br/>
+    They do say, if you love your job, you've never really worked, so I can safely say, I've
+    never
+    really worked. <br/>
+    I consider myself lucky to of found my passion early, I know many people are struggling but
+    I'd say the biggest thing to help me find my passion is travel!
+</p>
+
+<p>
+    You see, I'm a curious person, I want to see the world, to experience a different life
+    style, a different culture. <br/>
+    My cousin once told me, the biggest education in life you'll ever get is to see the world.
+    <br/>
+    He was definitely right! <br/>
+</p>
+
+<p>
+    This brings me to Thailand, I recently moved here and it's been the best decision I've ever
+    made to date. <br/>
+    Thailand is an absolutely wonderful country, it has warm weather all year round, amazing food and the people,
+    oh the people, they call it the land of smiles for a reason, I've never felt more welcome
+    than in Thailand. <br/>
+    I can definitely see myself staying here for the next few years at least, I'll get to travel
+    around Asia whilst living in Thailand.
+    It's nice to have a home so for me, Thailand is perfect.
+</p>
