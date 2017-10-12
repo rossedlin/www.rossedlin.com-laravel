83601bb6195b115351293a8137b717add354efb4
Ross Edlin <rossedlin@gmail.com>
2017-10-12 17:30:15 +0700

    fix contact page

--- app/Http/Controllers/Contact.php
+++ app/Http/Controllers/Contact.php
@@ -8,6 +8,7 @@ use \Cryslo\Object;
 
 /**
  * Created by PhpStorm.
+ *
  * @author  Ross Edlin <contact@rossedlin.com>
  *
  * Date: 09/09/2017
@@ -41,16 +42,36 @@ class Contact extends _Web
 			'message' => Core\Request::post('message'),
 		]);
 
+		/**
+		 * Build the email.
+		 */
+		$content = Core\Request::post('name') . " just sent you a contact request on www.rossedlin.com<br />
+		<br />
+		" . Core\Request::post('email') ."<br />
+		<br />
+		" . Core\Request::post('message');
+
 		$transport = \Swift_SmtpTransport::newInstance(env('MAIL_HOST'), env('MAIL_PORT'));
 
 		$transport->setUsername(env('MAIL_USERNAME'));
 		$transport->setPassword(env('MAIL_PASSWORD'));
 
+		$body = Core\Email::getTemplate('Default', [
+			'title'      => 'Ross Edlin',
+			'logo_href'  => 'https://cfmedia.deadline.com/2016/07/logo-tv-logo.png',
+			'logo_width' => '100',
+			'logo_alt'   => 'Ross Edlin',
+			'content'    => $content,
+			'year'       => date('Y'),
+		]);
+
 		$email = new Object\Email();
 		$email->setTransport($transport);
+		$email->setContentType(Core\Consts::CONTENT_TYPE_HTML);
 		$email->setTo('contact@rossedlin.com');
+		$email->setFromName('RossEdlin.com');
 		$email->setFrom('noreply@rossedlin.com');
-		$email->setBody("efwefw");
+		$email->setBody($body);
 
 		try
 		{
