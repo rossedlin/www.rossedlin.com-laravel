85168d3c1eb081f424769cb70c25400bdde51af9
Ross Edlin <rossedlin@gmail.com>
2017-10-12 13:12:10 +0700

    add - profile / history

---
+++ resources/parts/sections/profile/history.blade.php
@@ -0,0 +1,239 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * @author Ross Edlin <contact@rossedlin.com>
+ *
+ * Date: 12/10/2017
+ * Time: 13:04
+ */
+?>
+<!-- Profle Content -->
+<div class="col-lg-9">
+    <!-- Timeline Box -->
+    <ul class="row u-timeline-v2-wrap list-unstyled">
+        <li class="col-md-12 g-mb-40">
+            <div class="row">
+                <!-- Timeline Date -->
+                <div class="col-md-3 text-md-right g-pt-20--md g-pr-40--md g-mb-20">
+                    <h5 class="h6 g-font-weight-700 mb-0">2012</h5>
+                    <h4 class="h4 g-font-weight-300">Founded</h4>
+                </div>
+                <!-- End Timeline Date -->
+
+                <!-- Timeline Content -->
+                <div class="col-md-9 g-orientation-left g-pl-40--md">
+                    <!-- Timeline Dot -->
+                    <div class="g-hidden-sm-down u-timeline-v2__icon g-top-35">
+                        <i class="d-block g-width-18 g-height-18 g-bg-primary g-brd-around g-brd-4 g-brd-gray-light-v4 rounded-circle"></i>
+                    </div>
+                    <!-- End Timeline Dot -->
+
+                    <article class="g-pos-rel g-bg-gray-light-v5 g-pa-30">
+                        <!-- Timeline Arrow -->
+                        <div class="g-hidden-sm-down u-triangle-inclusive-v1--right g-top-30 g-z-index-2">
+                            <div class="u-triangle-inclusive-v1--right__back g-brd-gray-light-v5-right"></div>
+                        </div>
+                        <div class="g-hidden-md-up u-triangle-inclusive-v1--top g-left-20 g-z-index-2">
+                            <div class="u-triangle-inclusive-v1--top__back g-brd-gray-light-v5-bottom"></div>
+                        </div>
+                        <!-- End Timeline Arrow -->
+
+                        <header class="g-brd-bottom g-brd-gray-light-v4 g-pb-10 g-mb-25">
+                            <h3 class="g-font-weight-300">Our First Step</h3>
+                        </header>
+
+                        <p class="lead g-mb-25">Winter purslane courgette pumpkin quandong komatsuna
+                            fennel green bean cucumber watercress cress avocado grape.</p>
+                        <img class="g-height-120 g-width-120 g-mb-25 g-mr-20"
+                             src="../../assets/img-temp/100x100/img11.jpg" alt="Image description">
+                        <img class="g-height-120 g-width-120 g-mb-25 g-mr-20"
+                             src="../../assets/img-temp/100x100/img12.jpg" alt="Image description">
+                        <img class="g-height-120 g-width-120 g-mb-25"
+                             src="../../assets/img-temp/100x100/img13.jpg" alt="Image description">
+                        <p class="lead mb-0">Pea cress potato sprouts wattle seed rutabaga okra yarrow
+                            green bean cucumber watercress cress avocado grape. Cabbage lentil cucumber
+                            chickpea sorrel gram garbanzo plantain lotus root bok choy squash cress
+                            potato.</p>
+                    </article>
+                </div>
+                <!-- End Timeline Content -->
+            </div>
+        </li>
+
+        <li class="col-md-12 g-mb-40">
+            <div class="row">
+                <!-- Timeline Date -->
+                <div class="col-md-3 text-md-right g-pt-20--md g-pr-40--md g-mb-20">
+                    <h5 class="h6 g-font-weight-700 mb-0">2015</h5>
+                    <h4 class="h4 g-font-weight-300">Achievements</h4>
+                </div>
+                <!-- End Timeline Date -->
+
+                <!-- Timeline Content -->
+                <div class="col-md-9 g-orientation-left g-pl-40--md">
+                    <!-- Timeline Dot -->
+                    <div class="g-hidden-sm-down u-timeline-v2__icon g-top-35">
+                        <i class="d-block g-width-18 g-height-18 g-bg-primary g-brd-around g-brd-4 g-brd-gray-light-v4 rounded-circle"></i>
+                    </div>
+                    <!-- End Timeline Dot -->
+
+                    <article class="g-pos-rel g-bg-gray-light-v5 g-pa-30">
+                        <!-- Timeline Arrow -->
+                        <div class="g-hidden-sm-down u-triangle-inclusive-v1--right g-top-30 g-z-index-2">
+                            <div class="u-triangle-inclusive-v1--right__back g-brd-gray-light-v5-right"></div>
+                        </div>
+                        <div class="g-hidden-md-up u-triangle-inclusive-v1--top g-left-20 g-z-index-2">
+                            <div class="u-triangle-inclusive-v1--top__back g-brd-gray-light-v5-bottom"></div>
+                        </div>
+                        <!-- End Timeline Arrow -->
+
+                        <header class="g-brd-bottom g-brd-gray-light-v3 g-pb-10 g-mb-25">
+                            <h3 class="g-font-weight-300">Our Achievements</h3>
+                        </header>
+
+                        <p class="lead g-mb-25">Sprouts wattle seed pumpkin quandong komatsuna fennel
+                            green bean cucumber watercress. Pea cress potato sprouts wattle seed
+                            rutabaga okra yarrow cress avocado grape.</p>
+                        <div class="row g-mb-15">
+                            <div class="col-md-5 g-mb-25 g-mb-0--md">
+                                <img class="img-fluid w-100"
+                                     src="../../assets/img-temp/400x270/img9.jpg"
+                                     alt="Image description">
+                            </div>
+                            <div class="col-md-7">
+                                <ul class="list-unstyled g-color-gray-dark-v4 g-mb-0">
+                                    <li class="d-flex g-mb-10">
+                                        <i class="icon-check g-color-primary g-mt-5 g-mr-10"></i> Based
+                                        on Botostrap 4
+                                    </li>
+                                    <li class="d-flex g-mb-10">
+                                        <i class="icon-check g-color-primary g-mt-5 g-mr-10"></i>
+                                        Responsive Bootstrap Template
+                                    </li>
+                                    <li class="d-flex g-mb-10">
+                                        <i class="icon-check g-color-primary g-mt-5 g-mr-10"></i>
+                                        Semantic HTML5/CSS3 Codes
+                                    </li>
+                                    <li class="d-flex g-mb-10">
+                                        <i class="icon-check g-color-primary g-mt-5 g-mr-10"></i> Great
+                                        SASS Architecture
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <p class="lead mb-0">Cabbage lentil cucumber chickpea sorrel gram garbanzo
+                            plantain lotus root bok choy squash cress potato.</p>
+                    </article>
+                </div>
+                <!-- End Timeline Content -->
+            </div>
+        </li>
+
+        <li class="col-md-12">
+            <div class="row">
+                <!-- Timeline Date -->
+                <div class="col-md-3 text-md-right g-pt-20--md g-pr-40--md g-mb-20">
+                    <h5 class="h6 g-font-weight-700 mb-0">2017</h5>
+                    <h4 class="h4 g-font-weight-300">Our Products</h4>
+                </div>
+                <!-- End Timeline Date -->
+
+                <!-- Timeline Content -->
+                <div class="col-md-9 g-orientation-left g-pl-40--md">
+                    <!-- Timeline Dot -->
+                    <div class="g-hidden-sm-down u-timeline-v2__icon g-top-35">
+                        <i class="d-block g-width-18 g-height-18 g-bg-primary g-brd-around g-brd-4 g-brd-gray-light-v4 rounded-circle"></i>
+                    </div>
+                    <!-- End Timeline Dot -->
+
+                    <article class="g-pos-rel g-bg-gray-light-v5 g-pa-30">
+                        <!-- Timeline Arrow -->
+                        <div class="g-hidden-sm-down u-triangle-inclusive-v1--right g-top-30 g-z-index-2">
+                            <div class="u-triangle-inclusive-v1--right__back g-brd-gray-light-v5-right"></div>
+                        </div>
+                        <div class="g-hidden-md-up u-triangle-inclusive-v1--top g-left-20 g-z-index-2">
+                            <div class="u-triangle-inclusive-v1--top__back g-brd-gray-light-v5-bottom"></div>
+                        </div>
+                        <!-- End Timeline Arrow -->
+
+                        <header class="g-brd-bottom g-brd-gray-light-v3 g-pb-10 g-mb-25">
+                            <h3 class="g-font-weight-300">Premium &amp; Freebie Products</h3>
+                        </header>
+
+                        <p class="lead g-mb-20">Sprouts wattle seed pumpkin quandong komatsuna fennel
+                            green bean cucumber watercress. Pea cress potato sprouts wattle seed
+                            rutabaga okra yarrow cress avocado grape.</p>
+
+                        <div class="row">
+                            <div class="col-sm-6 g-mb-20">
+                                <div class="d-flex">
+                            <span class="u-icon-v3 g-bg-teal g-color-white g-mt-2 g-mr-15">
+                                <i class="icon-directions"></i>
+                              </span>
+
+                                    <div class="g-line-height-1">
+                                        <h4 class="h5 g-font-weight-300">Overal Visits</h4>
+                                        <div class="js-counter g-color-gray-dark-v4 g-font-size-18"
+                                             data-comma-separated="true">52147
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-6 g-mb-20">
+                                <div class="d-flex">
+                            <span class="u-icon-v3 g-bg-pink g-color-white g-mt-2 g-mr-15">
+                                <i class="icon-layers"></i>
+                              </span>
+
+                                    <div class="g-line-height-1">
+                                        <h4 class="h5 g-font-weight-300">Layouts</h4>
+                                        <div class="js-counter g-color-gray-dark-v4 g-font-size-18"
+                                             data-comma-separated="true">324056
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-sm-6 g-mb-20 g-mb-0--sm">
+                                <div class="d-flex">
+                            <span class="u-icon-v3 g-bg-aqua g-color-white g-mt-2 g-mr-15">
+                                <i class="icon-trophy"></i>
+                              </span>
+
+                                    <div class="g-line-height-1">
+                                        <h4 class="h5 g-font-weight-300">New Features</h4>
+                                        <div class="js-counter g-color-gray-dark-v4 g-font-size-18"
+                                             data-comma-separated="true">15147
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-6">
+                                <div class="d-flex">
+                            <span class="u-icon-v3 g-bg-purple g-color-white g-mt-2 g-mr-15">
+                                <i class="icon-badge"></i>
+                              </span>
+
+                                    <div class="g-line-height-1">
+                                        <h4 class="h5 g-font-weight-300">Messages</h4>
+                                        <div class="js-counter g-color-gray-dark-v4 g-font-size-18"
+                                             data-comma-separated="true">37283
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+                <!-- End Timeline Content -->
+            </div>
+        </li>
+    </ul>
+    <!-- End Timeline Box -->
+</div>
+<!-- End Profle Content -->
