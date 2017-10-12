d645f24a80b68fb70eee1d1a8888dab1b37c00af
Ross Edlin <rossedlin@gmail.com>
2017-10-12 15:18:06 +0700

    extract out - experience + education

---
+++ resources/parts/sections/about/education.blade.php
@@ -0,0 +1,158 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * @author Ross Edlin <contact@rossedlin.com>
+ *
+ * Date: 12/10/2017
+ * Time: 15:17
+ */
+?>
+<!-- Education Timeline -->
+<div class="card border-0 rounded-0 g-mb-40">
+    <!-- Panel Header -->
+    <div class="card-header d-flex align-items-center justify-content-between g-bg-gray-light-v5 border-0 g-mb-15">
+        <h3 class="h6 mb-0">
+            <i class="icon-graduation g-pos-rel g-top-1 g-mr-5"></i> Education
+        </h3>
+        <div class="dropdown g-mb-10 g-mb-0--md">
+                  <span class="d-block g-color-primary--hover g-cursor-pointer g-mr-minus-5 g-pa-5"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      <i class="icon-options-vertical g-pos-rel g-top-1"></i>
+                    </span>
+            <div class="dropdown-menu dropdown-menu-right rounded-0 g-mt-10">
+                <a class="dropdown-item g-px-10" href="#">
+                    <i class="icon-layers g-font-size-12 g-color-gray-dark-v5 g-mr-5"></i> Projects
+                </a>
+                <a class="dropdown-item g-px-10" href="#">
+                    <i class="icon-wallet g-font-size-12 g-color-gray-dark-v5 g-mr-5"></i> Wallets
+                </a>
+                <a class="dropdown-item g-px-10" href="#">
+                    <i class="icon-fire g-font-size-12 g-color-gray-dark-v5 g-mr-5"></i> Reports
+                </a>
+                <a class="dropdown-item g-px-10" href="#">
+                    <i class="icon-settings g-font-size-12 g-color-gray-dark-v5 g-mr-5"></i> Users Setting
+                </a>
+
+                <div class="dropdown-divider"></div>
+
+                <a class="dropdown-item g-px-10" href="#">
+                    <i class="icon-plus g-font-size-12 g-color-gray-dark-v5 g-mr-5"></i> View More
+                </a>
+            </div>
+        </div>
+    </div>
+    <!-- End Panel Header -->
+
+    <!-- Panel Body -->
+    <div class="js-scrollbar card-block u-info-v1-1 g-bg-white-gradient-v1--after g-height-300 g-pa-0">
+        <ul class="row u-timeline-v2-wrap list-unstyled">
+            <li class="col-md-12 g-brd-bottom g-brd-0--md g-brd-gray-light-v4 g-pb-30 g-pb-0--md g-mb-30 g-mb-0--md">
+                <div class="row">
+                    <!-- Timeline Date -->
+                    <div class="col-md-3 align-self-center text-md-right g-pr-40--md g-mb-20 g-mb-0--md">
+                        <h4 class="h5 g-font-weight-300">MBA</h4>
+                        <h5 class="h6 g-font-weight-300 mb-0">2008 - 2010</h5>
+                    </div>
+                    <!-- End Timeline Date -->
+
+                    <!-- Timeline Content -->
+                    <div class="col-md-9 align-self-center g-orientation-left g-pl-40--md">
+                        <!-- Timeline Dot -->
+                        <div class="g-hidden-sm-down u-timeline-v2__icon g-top-35">
+                            <i class="d-block g-width-18 g-height-18 g-bg-primary g-brd-around g-brd-4 g-brd-gray-light-v4 rounded-circle"></i>
+                        </div>
+                        <!-- End Timeline Dot -->
+
+                        <article class="g-pa-10--md">
+                            <h3 class="h4 g-font-weight-300">Imperial College London</h3>
+                            <p class="mb-0">Winter purslane courgette pumpkin quandong komatsuna fennel green bean
+                                cucumber. Pea cress potato sprouts wattle seed rutabaga.</p>
+                        </article>
+                    </div>
+                    <!-- End Timeline Content -->
+                </div>
+            </li>
+            <li class="col-md-12 g-brd-bottom g-brd-0--md g-brd-gray-light-v4 g-pb-30 g-pb-0--md g-mb-30 g-mb-0--md">
+                <div class="row">
+                    <!-- Timeline Date -->
+                    <div class="col-md-3 align-self-center text-md-right g-pr-40--md g-mb-20 g-mb-0--md">
+                        <h4 class="h5 g-font-weight-300">Bachelor of IT</h4>
+                        <h5 class="h6 g-font-weight-300 mb-0">2004 - 2008</h5>
+                    </div>
+                    <!-- End Timeline Date -->
+
+                    <!-- Timeline Content -->
+                    <div class="col-md-9 align-self-center g-orientation-left g-pl-40--md">
+                        <!-- Timeline Dot -->
+                        <div class="g-hidden-sm-down u-timeline-v2__icon g-top-35">
+                            <i class="d-block g-width-18 g-height-18 g-bg-primary g-brd-around g-brd-4 g-brd-gray-light-v4 rounded-circle"></i>
+                        </div>
+                        <!-- End Timeline Dot -->
+
+                        <article class="g-pa-10--md">
+                            <h3 class="h4 g-font-weight-300">MIT University</h3>
+                            <p class="mb-0">Winter purslane courgette pumpkin quandong komatsuna fennel green bean
+                                cucumber. Pea cress potato sprouts wattle seed rutabaga.</p>
+                        </article>
+                    </div>
+                    <!-- End Timeline Content -->
+                </div>
+            </li>
+            <li class="col-md-12 g-brd-bottom g-brd-0--md g-brd-gray-light-v4 g-pb-30 g-pb-0--md g-mb-30 g-mb-0--md">
+                <div class="row">
+                    <!-- Timeline Date -->
+                    <div class="col-md-3 align-self-center text-md-right g-pr-40--md g-mb-20 g-mb-0--md">
+                        <h4 class="h5 g-font-weight-300">High School</h4>
+                        <h5 class="h6 g-font-weight-300 mb-0">2001 - 2004</h5>
+                    </div>
+                    <!-- End Timeline Date -->
+
+                    <!-- Timeline Content -->
+                    <div class="col-md-9 align-self-center g-orientation-left g-pl-40--md">
+                        <!-- Timeline Dot -->
+                        <div class="g-hidden-sm-down u-timeline-v2__icon g-top-35">
+                            <i class="d-block g-width-18 g-height-18 g-bg-primary g-brd-around g-brd-4 g-brd-gray-light-v4 rounded-circle"></i>
+                        </div>
+                        <!-- End Timeline Dot -->
+
+                        <article class="g-pa-10--md">
+                            <h3 class="h4 g-font-weight-300">Chicago High School</h3>
+                            <p class="mb-0">Winter purslane courgette pumpkin quandong komatsuna fennel green bean
+                                cucumber. Pea cress potato sprouts wattle seed rutabaga.</p>
+                        </article>
+                    </div>
+                    <!-- End Timeline Content -->
+                </div>
+            </li>
+            <li class="col-md-12">
+                <div class="row">
+                    <!-- Timeline Date -->
+                    <div class="col-md-3 align-self-center text-md-right g-pr-40--md g-mb-20 g-mb-0--md">
+                        <h4 class="h5 g-font-weight-300">Elementary School</h4>
+                        <h5 class="h6 g-font-weight-300 mb-0">2001 - 1992</h5>
+                    </div>
+                    <!-- End Timeline Date -->
+
+                    <!-- Timeline Content -->
+                    <div class="col-md-9 align-self-center g-orientation-left g-pl-40--md">
+                        <!-- Timeline Dot -->
+                        <div class="g-hidden-sm-down u-timeline-v2__icon g-top-35">
+                            <i class="d-block g-width-18 g-height-18 g-bg-primary g-brd-around g-brd-4 g-brd-gray-light-v4 rounded-circle"></i>
+                        </div>
+                        <!-- End Timeline Dot -->
+
+                        <article class="g-pa-10--md">
+                            <h3 class="h4 g-font-weight-300">New York Elementary School</h3>
+                            <p class="mb-0">Winter purslane courgette pumpkin quandong komatsuna fennel green bean
+                                cucumber. Pea cress potato sprouts wattle seed rutabaga.</p>
+                        </article>
+                    </div>
+                    <!-- End Timeline Content -->
+                </div>
+            </li>
+        </ul>
+    </div>
+    <!-- End Panel Body -->
+</div>
+<!-- End Education Timeline -->
