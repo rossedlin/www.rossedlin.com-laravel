fd425846c8fff77fb4b88d990b251d6cfc5aba26
Ross Edlin <rossedlin@gmail.com>
2018-08-06 15:03:39 +0100

    refactor

--- app/Http/Controllers/_Web.php
+++ app/Http/Controllers/_Web.php
@@ -23,53 +23,48 @@ use \App\Objects;
  */
 abstract class _Web extends BaseController
 {
-	use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-	/**
-	 * Redis Keys
-	 */
-	const WORDPRESS_CACHE = 'feed-wordpress';
-	const WORDPRESS_URL   = 'https://wordpress.rossedlin.com';
-	const WORDPRESS_DATE  = 'jS M Y';
+    /**
+     * Redis Keys
+     */
+    const WORDPRESS_CACHE = 'feed-wordpress';
+    const WORDPRESS_URL   = 'https://wordpress.rossedlin.com';
+    const WORDPRESS_DATE  = 'jS M Y';
 
-	/**
-	 * @var Objects\Web\Meta $meta
-	 */
-	protected $meta;
+    /**
+     * @var Objects\Web\Meta $meta
+     */
+    protected $meta;
 
-	/**
-	 * @var Objects\Page $page
-	 */
-	protected $page;
+    /**
+     * @var Objects\Page $page
+     */
+    protected $page;
 
-	/**
-	 * @var array $data
-	 */
-	protected $data = [];
+    /**
+     * @var array $data
+     */
+    protected $data = [];
 
-	/**
-	 * PageController constructor.
-	 */
-	public function __construct()
-	{
-//        $feed            = new Feed\Wordpress();
-//        $wordpress = $feed->getFromUrl("https://wordpress.cuttingweb.co.uk/feed/");
+    /**
+     * PageController constructor.
+     */
+    public function __construct()
+    {
+        $this->meta = new Objects\Web\Meta();
+        $this->meta->setTitle("Ross Edlin");
+        $this->meta->setSiteName("Ross Edlin");
+        $this->meta->setDescription("Follow Ross Edlin's blog, talking about new technology, travel, photography.");
+        $this->meta->setKeywords('ross edlin, ross, edlin, technology, travel, photography');
+        $this->meta->setUrl(url('/'));
 
-		$this->meta = new Objects\Web\Meta();
-		$this->meta->setTitle("Ross Edlin");
-		$this->meta->setSiteName("Ross Edlin");
-		$this->meta->setDescription("Follow Ross Edlin's blog, talking about new technology, travel, photography.");
-		$this->meta->setKeywords('ross edlin, ross, edlin, technology, travel, photography');
-		$this->meta->setUrl(url('/'));
+        $this->page = new Objects\Page([]);
 
-		$this->page = new Objects\Page([
-//            'title' => $wordpress->getTitle(),
-		]);
-
-		$this->data = [
-			'meta'         => $this->meta,
-			'page'         => $this->page,
-			'latest_posts' => WordPress\Api::getLatestPosts(),
-		];
-	}
+        $this->data = [
+            'meta'         => $this->meta,
+            'page'         => $this->page,
+            'latest_posts' => WordPress\Api::getLatestPosts(),
+        ];
+    }
 }
